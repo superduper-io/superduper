cd8a2d7f27436d6e69c5c2573e0b8cb26a09162e
Duncan Blythe <duncan@superduperdb.com>
2023-01-08 19:59:39 +0100

    removed docs

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath('../superduperdb'))
+
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'SuperDuperDB'
+copyright = u'Duncan Blythe duncan@superduperdb.com'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+autoclass_content = "both"
+
+html_theme = 'sphinx_rtd_theme'

46d9bff622fed648701922738db938808b96ba8b
Duncan Blythe <d@lf1.io>
2023-01-24 13:54:32 +0100

    debugging example

--- examples/models.py
+++ examples/models.py
@@ -1,3 +1,5 @@
+import re
+
 from clip import load as load_clip, tokenize as clip_tokenize
 import torch
 
@@ -18,4 +20,23 @@ class CLIP(torch.nn.Module):
         if len(r.shape) == 2:
             return self.model.encode_text(r)
         return self.model.encode_image(r)
-...
+
+
+class AverageOfGloves:
+    def __init__(self, embeddings, index):
+        self.embeddings = embeddings
+        self.index = index
+        self.lookup = dict(zip(self.index, range(len(self.index))))
+
+    def preprocess(self, sentence):
+        if isinstance(sentence, list):
+            sentence = ' '.join(sentence)
+        cleaned = re.sub('[^a-z0-9 ]', ' ',  sentence.lower())
+        cleaned = re.sub('[ ]+', ' ',  cleaned)
+        words = cleaned.split()
+        words = [x for x in words if x in self.index]
+        if not words:
+            return torch.ones(50).type(torch.float)
+        ix = list(map(self.lookup.__getitem__, words))
+        vectors = self.embeddings[ix, :]
+        return vectors.mean(0)

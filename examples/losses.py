fbf7f84f83f7c157ec865864b68f3d51d09e0fe7
Duncan Blythe <d@lf1.io>
2023-02-10 09:59:34 -0600

    pre finished example

--- examples/losses.py
+++ examples/losses.py
@@ -8,15 +8,13 @@ def ranking_loss(x, y):
     return -torch.nn.functional.log_softmax(similarities, dim=1).diag().mean()
 
 
-class AutoRegressiveLoss:
-    def __init__(self, stop_token):
-        self.stop_token = stop_token
-
-    def __call__(self, x, y):
-        x = x.transpose(2, 1)
-        not_stops = (y != self.stop_token).type(torch.long)
-        losses = torch.nn.functional.cross_entropy(x, y)
-        normalizing_factors = not_stops.sum(axis=1).unsqueeze(1)
-        fractional_losses = (losses * not_stops).div(normalizing_factors).sum(axis=1)
-        return fractional_losses
+def auto_regressive_loss(x, y):
+    # start token = x.shape[2] - 2, stop_token = x.shape[2] - 1 (by convention)
+    stop_token = x.shape[2] - 1
+    x = x.transpose(2, 1)
+    not_stops = (y != stop_token).type(torch.long)
+    losses = torch.nn.functional.cross_entropy(x, y)
+    normalizing_factors = not_stops.sum(axis=1).unsqueeze(1)
+    fractional_losses = (losses * not_stops).div(normalizing_factors).sum(axis=1)
+    return fractional_losses
 

7c356389084b86e96c7c5bbfee87ef5cd4448828
Duncan Blythe <duncan@superduperdb.com>
2023-05-11 18:31:07 +0200

    fixed broken cluster tests

--- superduperdb/datalayer/base/imports.py
+++ superduperdb/datalayer/base/imports.py
@@ -10,7 +10,7 @@ def get_database_from_database_type(database_type, database_name):
     :param database_type: type of database (supported: ['mongodb'])
     :param database_name: name of database
     """
-    module = importlib.import_module(f'superduperdb.dbs.{database_type}.client')
+    module = importlib.import_module(f'superduperdb.datalayer.{database_type}.client')
     client_cls = getattr(module, 'SuperDuperClient')
     client = client_cls(**cf.get(database_type, {}))
     return client.get_database_from_name(database_name)
--- superduperdb/misc/logger.py
+++ superduperdb/misc/logger.py
@@ -5,3 +5,6 @@ cf_logging = cf.get('logging', {})
 
 logging.basicConfig(level=getattr(logging, cf_logging.get('level', 'DEBUG')),
                     **{k: v for k, v in cf_logging.items() if k != 'level'})
+
+logging.getLogger("distributed").propagate = True
+

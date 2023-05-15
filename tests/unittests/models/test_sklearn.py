3eef3a45953433f1ef3a4826b84ee78f504433ba
Duncan Blythe <duncan@superduperdb.com>
2023-05-15 12:39:19 +0200

    fixed tests - output types, dask imports
    Former-commit-id: 1766ba0b83c363a0f4ce86e1065709ce0ffa2b34

--- tests/unittests/models/test_sklearn.py
+++ tests/unittests/models/test_sklearn.py
@@ -8,7 +8,7 @@ def test_pipeline(random_arrays, int64):
     y = (numpy.random.rand(100) > 0.5).astype(int)
     est = SVC()
     est.fit(X, y)
-    random_arrays.create_model('test_sklearn', est, type='int64')
+    random_arrays.create_model('test_sklearn', est)
     pl = random_arrays.models['test_sklearn']
     print(pl)
     random_arrays.create_watcher('test_sklearn', key='x')

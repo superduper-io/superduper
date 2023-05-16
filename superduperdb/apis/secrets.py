757840641b369bf49d23726777fa96b8fe9c16ba
Duncan Blythe <duncan@superduperdb.com>
2023-05-16 07:03:44 +0200

    created openai demo code - need retries/ asyncio etc.
    Former-commit-id: bef6de99aae44615a35ba4bb2e8900f14513d52e

--- superduperdb/apis/secrets.py
+++ superduperdb/apis/secrets.py
@@ -1,32 +1,10 @@
-import os
+import importlib
+
+from superduperdb import cf
+
+api_cf = cf.get('apis', {'providers': {}})
 
 
-class CallableWithSecret:
-    """
-    Class allowing functions to use secrets which are then saved separately/ or potentially
-    not at all.
-    """
-    def __init__(self, secrets):
-        """
-        :param secrets: dictionary of secrets - are added as environment variables
-        """
-        self._secrets = None
-        self.secrets = secrets
-
-    @property
-    def secrets(self):
-        return self._secrets
-
-    @secrets.setter
-    def secrets(self, value):
-        self._secrets = value
-        if value is None:
-            return
-        self._set_envs()
-
-    def _set_envs(self):
-        for k in self.secrets:
-            os.environ[k] = self.secrets[k]
-
-    def __call__(self, *args, **kwargs):
-        raise NotImplementedError
+def client_init():
+    for provider in api_cf.get('providers', {}):
+        importlib.import_module(f'superduperdb.apis.{provider}.init').init_fn()

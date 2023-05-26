406efa8512607828571784f075a047fe5be90b5a
Duncan Blythe <duncan@superduperdb.com>
2023-05-26 15:41:46 +0200

    Add more comments to explain base query classes
    Former-commit-id: 855d69f18e476d05f46f965a721302861d744c83

--- superduperdb/datalayer/base/query.py
+++ superduperdb/datalayer/base/query.py
@@ -6,27 +6,39 @@ from superduperdb.misc.serialization import convert_from_types_to_bytes
 
 
 class Select(ABC):
+    """
+    Abstract base class, encapsulating Select database queries/ datalayer reads.
+    This allows the concrete implementation of each datalayer to differ substantially on
+    stored properties necessary for querying the DB.
+    """
 
     @property
     @abstractmethod
     def is_trivial(self) -> bool:
+        # Determines when a select statement is "just" select everything.
+        # For example, in SQL: "FROM my_table SELECT *"
+        # For example, in MongoDB: "collection.find()"
         pass
 
     @property
     @abstractmethod
     def select_only_id(self) -> 'Select':
+        # Converts the query into a query which only returns the id of each column/ document.
         pass
 
     @abstractmethod
     def select_using_ids(self, ids):
+        # Converts the query into a query which sub-selects from the ids specified.
         pass
 
     @abstractmethod
     def add_fold(self, fold: str) -> 'Select':
+        # Converts the query into a query which sub-selects based on the specified tag "fold"
         pass
 
 
 class Insert(ABC):
+    # must implement attribute/ property self.documents
 
     def _to_raw_documents(self, types, type_lookup) -> List[Any]:
         raw_documents = []
@@ -35,17 +47,21 @@ class Insert(ABC):
         return raw_documents
 
     @abstractmethod
-    def to_raw(self, types, type_lookup):
+    def to_raw(self, types, type_lookup) -> 'Insert':
+        # converts the Insert object into an equivalent insert object, but where the component
+        # types are dumped as bytes
         pass
 
     @property
     @abstractmethod
     def table(self):
+        # extracts the table name from the object
         pass
 
     @property
     @abstractmethod
     def select_table(self) -> Select:
+        # returns a Select object which selects the table into which the insert was inserted
         pass
 
 
@@ -57,14 +73,19 @@ class Update(ABC):
 
     @abstractmethod
     def to_raw(self, types, type_lookup):
+        # converts the Update object into an equivalent Update object, but where the component
+        # types are dumped as bytes in the update
         pass
 
     @property
     @abstractmethod
     def select(self):
+        # converts the update object to a Select object, which selects where the update was made
         pass
 
     @property
     @abstractmethod
     def select_ids(self):
+        # converts the update object to a Select object, which selects where the update was made
+        # and returns only ids
         pass

04941c1b685f823f65e06a631c263ecf7498d149
Tom Ritchford <tom@swirly.com>
2023-06-04 15:57:11 +0200

    Add flag --schema to `sddb config`

--- superduperdb/cli/config.py
+++ superduperdb/cli/config.py
@@ -1,8 +1,16 @@
 from . import command
 from superduperdb import CFG
-import json
+from typer import Option
 
 
 @command(help='Print all the SuperDuperDB configs as JSON')
-def config():
-    print(json.dumps(CFG.dict(), indent=2))
+def config(
+    schema: bool = Option(
+        False,
+        '--schema',
+        '-s',
+        help='If set, print the JSON schema for the model'
+    ),
+):
+    json = CFG.schema_json if schema else CFG.json
+    print(json(indent=2))

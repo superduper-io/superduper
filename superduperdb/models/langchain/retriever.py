d9225d45fc5d8bb46bbf47e5299154df5095b3c5
Duncan Blythe <d@lf1.io>
2023-06-07 11:00:24 -0400

    Add langchain model and unittest

---
+++ superduperdb/models/langchain/retriever.py
@@ -0,0 +1,123 @@
+from functools import cached_property
+from typing import Union, List
+
+from langchain.base_language import BaseLanguageModel
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chains.base import Chain
+from langchain.schema import BaseRetriever, Document
+
+from superduperdb.core.base import Placeholder
+from superduperdb.core.model import Model
+from superduperdb.core.vector_index import VectorIndex
+
+
+class LangchainRetriever(BaseRetriever):
+    """
+    Retriever wrapping SuperDuperDB vector search, for compatibility with LangChain
+    RetrievalQAWithSourcesChain
+
+    :param key: Key/field/ column to search
+    :param vector_index: Vector-index to use for search
+    :param n: Number of documents to use for context
+    """
+
+    def __init__(
+        self,
+        key: str,
+        vector_index: VectorIndex,
+        n: int = 100,
+    ):
+        self.vector_index = vector_index
+        self.key = key
+        self.n = n
+
+    def get_relevant_documents(self, query: str) -> List[Document]:
+        document_to_search = {self.key: query}
+        ids = self.vector_index.get_nearest(document_to_search, n=self.n)[0]
+        select = self.vector_index.select.select_using_ids(ids)
+        out = list(self.vector_index.database.select(select))
+        out = [
+            Document(
+                page_content=x[self.key],
+                metadata={'source': x[self.vector_index.database.id_field]},
+            )
+            for x in out
+        ]
+        return out
+
+    async def aget_relevant_documents(self, query: str) -> List[Document]:
+        raise NotImplementedError
+
+
+class ChainWrapper(Model):
+    """
+    Wrapper to serialize and apply langchain Chain
+
+    :params chain: Langchain Chain object
+    :param identifier: unique ID
+    """
+
+    def __init__(self, chain: Chain, identifier: str):
+        msg = (
+            "Chains which include a retrieval pass are handled by "
+            f"{DBQAWithSourcesChain}"
+        )
+        assert not isinstance(chain, RetrievalQAWithSourcesChain), msg
+        super().__init__(chain, identifier=identifier)
+
+    def predict_one(self, input):
+        return self.object.run(input)
+
+
+class DBQAWithSourcesChain(Model):
+    """
+    Model which applies ``langchain.chains.RetrievalQAWithSourcesChain`` to the
+    database, levering vector search.
+
+    :param identifier: Unique ID
+    :param key: Field/ column to apply chain
+    :param llm: LangChain language model
+    :param vector_index: VectorIndex component to apply
+    :param chain_type: important parameter to RetrievalQAWithSourcesChain
+    :param n: Number of documents to retrieve as seed for chain
+    """
+
+    def __init__(
+        self,
+        identifier: str,
+        key: str,
+        llm: BaseLanguageModel,
+        vector_index: Union[VectorIndex, str],
+        chain_type: str = 'stuff',
+        n: int = 5,
+    ):
+        super().__init__(llm, identifier=identifier)
+        self.chain_type = chain_type
+        self._retrieval_chain = None
+        self.vector_index = (
+            vector_index
+            if isinstance(vector_index, VectorIndex)
+            else Placeholder(variety='vector_index', identifier=vector_index)
+        )
+        self.key = key
+        self.n = n
+
+    @cached_property
+    def retriever(self) -> LangchainRetriever:
+        assert not isinstance(self.vector_index, Placeholder)
+        return LangchainRetriever(self.key, self.vector_index, self.n)
+
+    @cached_property
+    def chain(self) -> Chain:
+        assert hasattr(self, 'retriever')
+        return RetrievalQAWithSourcesChain.from_chain_type(
+            llm=self.object,
+            retriever=self.retriever,
+            chain_type=self.chain_type,
+        )
+
+    def predict_one(self, question):
+        return self.chain(question)
+
+    def predict(self):
+        raise NotImplementedError
--- superduperdb/models/sentence_transformers/wrapper.py
+++ superduperdb/models/sentence_transformers/wrapper.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import Optional, Union
 
 from sentence_transformers import SentenceTransformer as _SentenceTransformer
 
 from superduperdb.core.model import Model
+from superduperdb.core.type import Type
 
 
 class SentenceTransformer(Model):
@@ -10,11 +11,12 @@ class SentenceTransformer(Model):
         self,
         model_name_or_path: Optional[str] = None,
         identifier: Optional[str] = None,
+        type: Optional[Union[str, Type]] = None,
     ):
         if identifier is None:
             identifier = model_name_or_path
         sentence_transformer = _SentenceTransformer(model_name_or_path)
-        super().__init__(sentence_transformer, identifier)
+        super().__init__(sentence_transformer, identifier, type=type)
 
     def predict_one(self, sentence, **kwargs):
         return self.object.encode(sentence)

d37321120926b12c594e58cad7b5f06879dbdc2f
Tom Ritchford <tom@swirly.com>
2023-06-12 13:25:01 +0200

    Implement server (see #116)

---
+++ tests/unittests/server/test_registry.py
@@ -0,0 +1,35 @@
+from superduperdb.server.registry import Registry
+from argparse import Namespace
+import typing as t
+import superduperdb as s
+
+
+def setup_registry(register):
+    class One(s.Serializable):
+        one = 'one'
+
+    class Two(One):
+        two = 'two'
+
+    class Three(One):
+        two = 'three'
+
+    class Object:
+        @register
+        def first(self, one: One) -> Two:
+            return Two(**one.dict())
+
+        @register
+        def second(self, one: One, three: Three) -> One:
+            return one
+
+    return Namespace(**locals())
+
+
+def test_registry():
+    registry = Registry()
+    test = setup_registry(registry.register)
+    assert registry.Parameter == t.Union[test.One, test.Three]
+    assert registry.Result == t.Union[test.One, test.Two]
+
+    return registry

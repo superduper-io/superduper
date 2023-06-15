a7389932746417308cf46f520c7bcedcb9ecd0fd
Tom Ritchford <tom@swirly.com>
2023-06-15 13:03:42 +0200

    Consolidate and clean openai code

--- superduperdb/apis/openai/init.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from superduperdb import CFG
-from superduperdb.misc.logger import logging
-
-
-def init_fn():
-    logging.info('Setting OpenAI api-key...')
-    os.environ['OPENAI_API_KEY'] = CFG.apis.providers['openai']['api_key']

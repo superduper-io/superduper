6aac3748c63db3be5d33f8d3d4fe9fc8453108e0
Duncan Blythe <d@lf1.io>
2023-06-15 10:05:06 -0400

    Add shape parameter to type, linking to vectors/ tensors

--- tests/unittests/core/test_documents.py
+++ tests/unittests/core/test_documents.py
@@ -4,10 +4,10 @@ import torch
 
 
 def test_document_encoding():
-    t = tensor(torch.float)
+    t = tensor(torch.float, shape=(20,))
 
     r = Document({'x': t(torch.randn(20))})
 
     print(r.encode())
 
-    Document.decode(r.encode(), types={'torch.float32': t})
+    Document.decode(r.encode(), types={'torch.float32[20]': t})

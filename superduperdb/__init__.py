52c95527c6f4fc35482533fc81d3584e3e5d904e
Tom Ritchford <tom@swirly.com>
2023-06-15 13:03:42 +0200

    Alias `s.misc.logging.logging` as `s.log`
    Former-commit-id: 7b45789022e1cf188316faaf544bd2902cb25df6

--- superduperdb/__init__.py
+++ superduperdb/__init__.py
@@ -1,14 +1,13 @@
 from .misc import config, configs
 from .misc.jsonable import JSONable
 from pathlib import Path
-import os
 
-__all__ = 'CFG', 'ICON', 'JSONable', 'ROOT', 'config'
+__all__ = 'CFG', 'ICON', 'JSONable', 'ROOT', 'config', 'log'
 
 CFG = configs.CONFIG.config
 ICON = '🔮'
 ROOT = Path(__file__).parent
 
-if openai := CFG.apis.providers.get('openai'):
-    assert openai.api_key
-    os.environ['OPENAI_API_KEY'] = openai.api_key
+from .misc import logger  # noqa: E402
+
+log = logger.logging

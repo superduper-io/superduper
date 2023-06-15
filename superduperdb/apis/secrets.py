5353feb1178eb51d2793e6ffbd228847de271b91
Tom Ritchford <tom@swirly.com>
2023-06-15 11:01:34 +0200

    Remove unused superduperdb/apis/secrets.py

--- superduperdb/apis/secrets.py
+++
@@ -1,7 +0,0 @@
-import importlib
-from superduperdb import CFG
-
-
-def client_init():
-    for provider in CFG.apis.providers:
-        importlib.import_module(f'superduperdb.apis.{provider}.init').init_fn()

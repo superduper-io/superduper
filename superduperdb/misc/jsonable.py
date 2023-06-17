8a30037ba0081b95b8160f0da5dc7b056af956b5
Tom Ritchford <tom@swirly.com>
2023-06-17 16:05:37 +0200

    Clean, document and add typing to JSONable

--- superduperdb/misc/jsonable.py
+++ superduperdb/misc/jsonable.py
@@ -1,28 +1,45 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+import sys
+import typing as t
 
-TYPE_ID = 'type_id'
-MODELS = set()
-TYPE_ID_TO_MODEL = {}
+__all__ = 'Factory', 'JSONable'
+
+TYPE_ID_ATTR = 'type_id'
 _NONE = object()
+
+
+def Factory(factory: t.Callable, **ka) -> t.Any:
+    return Field(default_factory=factory, **ka)
 
 
 class JSONable(BaseModel):
     """
     JSONable is the base class for all superduperdb classes that can be
-    converted to and from JSON using the pydantic serialization system
+    converted to and from JSON
     """
 
-    def __init_subclass__(cls, *a, **ka):
-        super().__init_subclass__(*a, **ka)
-        if (type_id := getattr(cls, TYPE_ID, _NONE)) is not _NONE:
-            if old_model := TYPE_ID_TO_MODEL.get(type_id):
-                raise ValueError(f'Duplicate type_id: old={old_model}, new={cls}')
-            TYPE_ID_TO_MODEL[type_id] = cls
+    class Config:
+        extra = 'forbid'  # Fail in deserializion if there are extra fields
 
-        MODELS.add(cls)
+    SUBCLASSES: t.ClassVar[t.Set[t.Type]] = set()
+    TYPE_ID_TO_CLASS: t.ClassVar[t.Dict[str, t.Type]] = {}
 
     def deepcopy(self) -> 'JSONable':
         return self.copy(deep=True)
 
-    class Config:
-        extra = 'forbid'
+    def __init_subclass__(cls, *a, **ka):
+        super().__init_subclass__(*a, **ka)
+        cls.SUBCLASSES.add(cls)
+
+        try:
+            type_id = getattr(cls, TYPE_ID_ATTR)
+        except AttributeError:
+            return
+
+        if not isinstance(type_id, str):
+            (type_id,) = type_id.__args__
+
+        if old_model := cls.TYPE_ID_TO_CLASS.get(type_id):
+            print(f'Duplicate type_id: old={old_model}, new={cls}', file=sys.stderr)
+
+        cls.TYPE_ID_TO_CLASS[type_id] = cls

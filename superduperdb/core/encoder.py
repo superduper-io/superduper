9a99f908b377ee63ca91300ebb1658dab0d1cd2e
Tom Ritchford <tom@swirly.com>
2023-06-17 19:04:43 +0200

    Modify superduperdb/core/encoder.py

--- superduperdb/core/encoder.py
+++ superduperdb/core/encoder.py
@@ -28,8 +28,8 @@ class Encoder(Component):
         shape: t.Optional[t.Tuple] = None,
     ):
         super().__init__(identifier)
-        self.encoder = encoder
-        self.decoder = decoder
+        self.encoder = encoder or _pickle_dump
+        self.decoder = decoder or _pickle_load
         self.shape = shape
 
     def __repr__(self):
@@ -38,19 +38,8 @@ class Encoder(Component):
         else:
             return f'Encoder[{self.identifier}/{self.version}]'
 
-    def decode(self, bytes):
-        if self.decoder is None:
-            return DataVar(
-                pickle.load(io.BytesIO(bytes)),
-                type=self.identifier,
-                encoder=self.encoder,
-            )
-        return DataVar(
-            self.decoder(bytes),
-            type=self.identifier,
-            encoder=self.encoder,
-            shape=self.shape,
-        )
+    def decode(self, b: bytes) -> t.Any:
+        return DataVar(self.decoder(b), type=self.identifier, encoder=self.encoder)
 
     def __call__(self, x):
         return DataVar(
@@ -59,3 +48,13 @@ class Encoder(Component):
             encoder=self.encoder,
             shape=self.shape,
         )
+
+
+def _pickle_load(x: bytes) -> t.Any:
+    return pickle.load(io.BytesIO(b))
+
+
+def _pickle_dump(x: t.Any) -> bytes:
+    f = io.BytesIO()
+    pickle.dump(x, f)
+    return f.getvalue()

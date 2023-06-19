cc67a18a4f01506875450eb03003d9390c456958
Duncan Blythe <d@lf1.io>
2023-06-19 14:30:37 -0400

    Create torch pipeline

--- superduperdb/models/torch/wrapper.py
+++ superduperdb/models/torch/wrapper.py
@@ -1,19 +1,189 @@
+import inspect
 import io
-<<<<<<< HEAD
-import typing as t
-=======
 from contextlib import contextmanager
-from typing import Optional, Callable
->>>>>>> 11538be (Wrap models instead of subclassing)
+from typing import Optional, Callable, Union, Dict, List
 
 import torch
 from torch.utils import data
 
+from superduperdb.core.base import Placeholder
 from superduperdb.core.documents import Document
-from superduperdb.core.encoder import Encodable
+from superduperdb.core.encoder import Encoder, Encodable
 from superduperdb.core.model import Model
 from superduperdb.misc import progress
 from superduperdb.models.torch.utils import device_of, to_device, eval
+
+
+class TorchPipeline:
+    """
+    Sklearn style PyTorch pipeline.
+
+    :param steps: List of ``sklearn`` style steps/ transforms
+    :param identifier: Unique identifier
+    :param collate_fn: Function for collating batches
+    """
+
+    def __init__(
+        self,
+        identifier,
+        steps,
+        collate_fn: Optional[Callable] = None,
+        is_batch: Optional[Callable] = None,
+        encoder: Optional[Union[Encoder, str]] = None,
+    ):
+        self.steps = steps
+        self.identifier = identifier
+        self.collate_fn = collate_fn
+        self._forward_sequential = None
+        self.is_batch = is_batch
+        self.type = (
+            encoder if isinstance(encoder, Encoder) else Placeholder('type', encoder)
+        )
+
+    def __repr__(self):
+        lines = [
+            'TorchPipeline(steps=[',
+            *[f'   {(s[0], s[1])}' for s in self.steps],
+            '])',
+        ]
+        return '\n'.join(lines)
+
+    def _test_if_batch(self, x):
+        if self.is_batch is not None:
+            return self.is_batch(x)
+        if hasattr(self.steps[0][1], '__call__'):
+            type = next(
+                iter(inspect.signature(self.steps[0][1].__call__).parameters.values())
+            ).annotation
+        else:
+            type = next(
+                iter(inspect.signature(self.steps[0][1]).parameters.values())
+            ).annotation
+        if type != inspect._empty:
+            return not isinstance(x, type)
+        return isinstance(x, list)
+
+    @property
+    def steps(self):
+        return self._steps
+
+    def preprocess(self, x):
+        for s in self.steps[: self._forward_mark]:
+            transform = s[1]
+            if hasattr(transform, 'transform'):
+                x = transform.transform(x)
+            else:
+                assert callable(transform)
+                x = transform(x)
+        return x
+
+    @property
+    def preprocess_pipeline(self):
+        return TorchPipeline(
+            identifier=f'{self.identifier}/preprocess',
+            steps=self.steps[: self._forward_mark],
+        )
+
+    @property
+    def forward_pipeline(self):
+        if self._forward_sequential is None:
+            forward_steps = self.steps[self._forward_mark : self._post_mark]
+            self._forward_sequential = torch.nn.Sequential(
+                *[s[1] for s in forward_steps]
+            )
+        return self._forward_sequential
+
+    def forward(self, x):
+        return self._forward_sequential(x)
+
+    @property
+    def postprocess_pipeline(self):
+        return TorchPipeline(
+            identifier=f'{self.identifier}/postprocess',
+            steps=self.steps[self._post_mark :],
+        )
+
+    def postprocess(self, x):
+        for s in self.steps[self._post_mark :]:
+            transform = s[1]
+            if hasattr(transform, 'transform'):
+                x = transform.transform(x)
+            else:
+                assert callable(transform)
+                x = transform(x)
+        return x
+
+    def predict(self, x, **kwargs):
+        if not self._test_if_batch(x):
+            return self._predict_one(x, **kwargs)
+        if self.preprocess_pipeline.steps:
+            inputs = BasicDataset(x, self.preprocess)
+            loader = torch.utils.data.DataLoader(
+                inputs, **kwargs, collate_fn=self.collate_fn
+            )
+        else:
+            loader = torch.utils.data.DataLoader(
+                x, **kwargs, collate_fn=self.collate_fn
+            )
+        out = []
+        for batch in progress.progressbar(loader, total=len(loader)):
+            batch = to_device(batch, device_of(self.forward_pipeline))
+            tmp = self.forward(batch)
+            tmp = to_device(tmp, 'cpu')
+            tmp = unpack_batch(tmp)
+            tmp = list(map(self.postprocess, tmp))
+            out.extend(tmp)
+        return out
+
+    def _predict_one(self, x, **kwargs):
+        with torch.no_grad(), eval(self.forward_pipeline):
+            x = self.preprocess(x)
+            x = to_device(x, device_of(self.forward_pipeline))
+            singleton_batch = create_batch(x)
+            output = self.forward(singleton_batch)
+            output = unpack_batch(output)[0]
+            return self.postprocess(output)
+
+    @contextmanager
+    def eval(self):
+        was_training = self.forward_pipeline.steps[0][1].training
+        try:
+            for s in self.forward_pipeline.steps:
+                s[1].eval()
+            yield
+        finally:
+            if was_training:
+                for s in self.forward_pipeline.steps:
+                    s[1].eval()
+
+    saving = eval
+
+    @steps.setter
+    def steps(self, value):
+        self._steps = value
+        try:
+            self._forward_mark = next(
+                i
+                for i, s in enumerate(value)
+                if isinstance(s[1], torch.nn.Module)
+                or isinstance(s[1], torch.jit.ScriptModule)
+            )
+        except StopIteration:
+            self._forward_mark = len(self.steps)
+
+        try:
+            self._post_mark = next(
+                len(value) - i
+                for i, s in enumerate(value[::-1])
+                if isinstance(s[1], torch.nn.Module)
+                or isinstance(s[1], torch.jit.ScriptModule)
+            )
+        except StopIteration:
+            self._post_mark = len(self.steps)
+
+    def parameters(self):
+        for s in self.forward_pipeline.steps:
+            yield from s[1].parameters()
 
 
 class TorchModel(Model):
@@ -21,19 +191,13 @@ class TorchModel(Model):
         self,
         object,
         identifier,
-        preprocess=None,
-        postprocess=None,
         encoder=None,
-        collate_fn: t.Optional[t.Callable] = None,
+        collate_fn: Optional[Callable] = None,
+        num_directions: Optional[Union[Dict, List, int]] = 2,
     ):
         Model.__init__(self, object, identifier, encoder=encoder)
-        if hasattr(self.object, 'preprocess') and preprocess is None:
-            preprocess = self.object.preprocess
-        if hasattr(self.object, 'postprocess') and postprocess is None:
-            postprocess = self.object.postprocess
-        self._preprocess = preprocess
-        self._postprocess = postprocess
         self.collate_fn = collate_fn
+        self.num_directions = num_directions
 
     def parameters(self):
         return self.object.parameters()
@@ -71,112 +235,63 @@ class TorchModel(Model):
                     io.BytesIO(state.pop('object_bytes'))
                 )
 
-    def predict_one(self, x, **kwargs):
+    def _predict_one(self, x):
         with torch.no_grad(), eval(self.object):
-            return apply_model(
-                self, x, single=True, collate_fn=self.collate_fn, **kwargs
-            )
+            if hasattr(self.object, 'preprocess'):
+                x = self.object.preprocess(x)
+            x = to_device(x, device_of(self.object))
+            singleton_batch = create_batch(x)
+            output = self.object(singleton_batch)
+            output = to_device(output, 'cpu')
+            args = unpack_batch(output)[0]
+            if hasattr(self.object, 'postprocess'):
+                args = self.object.postprocess(args)
+            return args
 
     def predict(self, x, **kwargs):
         with torch.no_grad(), eval(self.object):
-            return apply_model(
-                self, x, single=False, collate_fn=self.collate_fn, **kwargs
-            )
-
-    def preprocess(self, x):
-        if self._preprocess is None:
-            return x
-        return self._preprocess(x)
-
-    def forward(self, *args, **kwargs):
-        return self.object(*args, **kwargs)
-
-    def postprocess(self, out):
-        if self._postprocess is None:
-            return out
-        return self._postprocess(out)
-
-
-def apply_model(
-    model, args, single=True, forward='forward', postprocess=True, **kwargs
-):
-    """
-    Apply model to args including pre-processing, forward pass and post-processing.
-
-    :param model: model object including methods *preprocess*, *forward* and
-                  *postprocess*
-    :param args: single or multiple data points over which to evaluate model
-    :param single: toggle to apply model to single or multiple (batched) datapoints.
-    :param forward: name of the forward pass
-    :param postprocess: toggle to ``False`` to get only forward outputs:w
-    :param kwargs: key, value pairs to be passed to dataloader
-
-    >>> from types import SimpleNamespace
-    >>> model = SimpleNamespace(
-    ...     preprocess=lambda x: x + 2,
-    ...     forward=torch.nn.Linear(16, 4),
-    ...     postprocess=lambda x: x.topk(1)[1].item()
-    ... )
-    >>> out = apply_model(model, torch.randn(16))
-    >>> isinstance(out, int)
-    True
-    >>> out = apply_model(model, [torch.randn(16), torch.randn(16)],
-                          single=False, batch_size=2)
-    >>> isinstance(out, list)
-    True
-    >>> len(out)
-    2
-
-    >>> model = SimpleNamespace(
-    ...     preprocess=lambda x: x + 2,
-    ...     postprocess=lambda x: torch.cat([x, x])
-    ... )
-    >>> out = apply_model(model, torch.randn(16))
-    >>> out.shape
-    torch.Size([32])
-    >>> out = apply_model(model, [torch.randn(16), torch.randn(16)], single=False)
-    >>> out[0].shape
-    torch.Size([32])
-
-    >>> model = SimpleNamespace(
-    ...     forward=torch.nn.Linear(16, 4),
-    ...     postprocess=lambda x: x.topk(1)[1].item()
-    ... )
-    >>> out = apply_model(model, [torch.randn(16), torch.randn(16)],
-                          single=False, batch_size=2)
-
-    """
-    if single:
-        if hasattr(model, 'preprocess'):
-            args = model.preprocess(args)
-        args = to_device(args, device_of(model.object))
-        if hasattr(model, forward):
-            singleton_batch = create_batch(args)
-            output = getattr(model, forward)(singleton_batch)
-            output = to_device(output, 'cpu')
-            args = unpack_batch(output)[0]
-        if postprocess and hasattr(model, 'postprocess'):
-            args = model.postprocess(args)
-        return args
-    else:
-        if hasattr(model, 'preprocess'):
-            inputs = BasicDataset(args, model.preprocess)
-            loader = torch.utils.data.DataLoader(inputs, **kwargs)
-        else:
-            loader = torch.utils.data.DataLoader(args, **kwargs)
-        out = []
-        for batch in progress.progressbar(loader, total=len(loader)):
-            batch = to_device(batch, device_of(model.object))
-            if hasattr(model, forward):
-                tmp = getattr(model, forward)(batch)
+            if not isinstance(x, list) and not test_if_batch(x, self.num_directions):
+                return self._predict_one(x)
+            if hasattr(self.object, 'preprocess'):
+                inputs = BasicDataset(x, self.object.preprocess)
+                loader = torch.utils.data.DataLoader(inputs, **kwargs)
+            else:
+                loader = torch.utils.data.DataLoader(x, **kwargs)
+            out = []
+            for batch in progress.progressbar(loader, total=len(loader)):
+                batch = to_device(batch, device_of(self.object))
+                tmp = self.object(batch)
                 tmp = to_device(tmp, 'cpu')
                 tmp = unpack_batch(tmp)
-            else:
-                tmp = unpack_batch(batch)
-            if postprocess and hasattr(model, 'postprocess'):
-                tmp = list(map(model.postprocess, tmp))
-            out.extend(tmp)
-        return out
+                if hasattr(self.object, 'postprocess'):
+                    tmp = list(map(self.object.postprocess, tmp))
+                out.extend(tmp)
+            return out
+
+
+def test_if_batch(x, num_directions: Union[Dict, int]):
+    """
+    :param x: item to test whether batch or singleton
+    :param num_directions: dictionary to test a leaf node in ``x`` whether batch or not
+
+    >>> test_if_batch(torch.randn(10), 2)
+    False
+    >>> test_if_batch(torch.randn(2, 10), 2)
+    True
+    >>> test_if_batch({'x': torch.randn(2, 10)}, {'x': 2})
+    True
+    >>> test_if_batch({'x': torch.randn(10)}, {'x': 2})
+    False
+    """
+    if isinstance(num_directions, int):
+        if len(x.shape) == num_directions:
+            return True
+        assert len(x.shape) == num_directions - 1
+        return False
+    else:
+        assert len(num_directions.keys()) == 1
+        key = next(iter(num_directions.keys()))
+        return test_if_batch(x[key], num_directions[key])
 
 
 class BasicDataset(data.Dataset):

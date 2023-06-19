dcd62250972150cc385f6351fd214b4a9bdbafaa
Duncan Blythe <duncan@superduperdb.com>
2023-06-19 15:04:54 -0400

    Modify sklearn pipeline wrapper
    Former-commit-id: 6c4bdd14076ffbc2ce1beb7a4e16c635d71e4261

--- superduperdb/models/sklearn/wrapper.py
+++ superduperdb/models/sklearn/wrapper.py
@@ -2,19 +2,40 @@ from sklearn.pipeline import Pipeline as BasePipeline
 from superduperdb.core.model import Model
 
 
+def postprocess(f):
+    f.superduper_postprocess = True
+    return f
+
+
 class Pipeline(Model):
     def __init__(
         self,
-        steps,
         identifier,
+        steps,
         memory=None,
         verbose=False,
-        postprocessor=None,
         encoder=None,
     ):
-        pipeline = BasePipeline(steps, memory=memory, verbose=verbose)
+        standard_steps = [
+            i
+            for i, s in enumerate(steps)
+            if not getattr(s[1], 'superduper_postprocess', False)
+        ]
+        postprocess_steps = [
+            i
+            for i, s in enumerate(steps)
+            if getattr(s[1], 'superduper_postprocess', False)
+        ]
+
+        if postprocess_steps:
+            msg = 'Postprocess steps must go after preprocess steps'
+            assert max(standard_steps) < min(postprocess_steps), msg
+
+        pipeline = BasePipeline(
+            [steps[i] for i in standard_steps], memory=memory, verbose=verbose
+        )
+        self.postprocess_steps = [steps[i] for i in postprocess_steps]
         Model.__init__(self, pipeline, identifier, encoder=encoder)
-        self.postprocessor = postprocessor
 
     def score(self, X, y=None, **predict_params):
         return self.object.score(X, y=y, **predict_params)
@@ -22,11 +43,14 @@ class Pipeline(Model):
     def fit(self, X, y):
         return self.object.fit(X, y)
 
-    def predict_one(self, X, **predict_params):
-        return self.object.predict([X], **predict_params)[0]
-
     def predict(self, X, **predict_params):
         out = self.object.predict(X, **predict_params).tolist()
-        if self.postprocessor is not None:
-            return [self.postprocessor(x) for x in out]
-        return list(out)
+        if self.postprocess_steps:
+            for s in self.postprocess_steps:
+                if hasattr(s[1], 'transform'):
+                    out = s[1].transform(s)
+                elif callable(s[1]):
+                    out = s[1](out)
+                else:
+                    raise Exception('Unexpected postprocessing transform')
+        return out

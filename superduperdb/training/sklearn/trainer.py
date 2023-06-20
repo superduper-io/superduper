5162b0699cf7ce523a739f2b1474fda7ae3329e0
Diogo Santos Martins <yogaxpto@gmail.com>
2023-06-20 10:46:11 +0100

    Add typing to superduperdb/training
    Former-commit-id: b65ee1aa1722876de7af4cb370dc6c30b7b78027

--- superduperdb/training/sklearn/trainer.py
+++ superduperdb/training/sklearn/trainer.py
@@ -1,3 +1,5 @@
+import typing as t
+
 from superduperdb.core.training_configuration import TrainingConfiguration
 from superduperdb.datalayer.base.build import build_datalayer
 from superduperdb.datalayer.base.query import Select
@@ -6,6 +8,9 @@ import numpy
 
 
 class SklearnTrainingConfiguration(TrainingConfiguration):
+    fit_params: t.Dict
+    predict_params: t.Dict
+
     def __init__(self, identifier, fit_params=None, predict_params=None):
         super().__init__(
             identifier, fit_params=fit_params or {}, predict_params=predict_params or {}

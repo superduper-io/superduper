9c5045ffbfdaa6c0c3da05f74da4202005375c87
Tom Ritchford <tom@swirly.com>
2023-06-24 19:52:35 +0200

    Serve from an instance of BaseDatabase
    Former-commit-id: 60728c0e2d694d0573ac5959158926be69ae4300

--- superduperdb/cli/serve.py
+++ superduperdb/cli/serve.py
@@ -1,8 +1,29 @@
 from . import command
+from superduperdb.datalayer.base import build
 from superduperdb.server.server import Server
-from tests.unittests.server.test_server import Object
+from threading import Thread
+import time
+import webbrowser
 
 
 @command(help='Start server')
-def serve():
-    Server().auto_run(Object())
+def serve(
+    open_page: bool = True,
+    open_delay: float = 0.5,
+):
+    s = Server()
+    db = build.build_datalayer()
+
+    s.register(db.select)
+
+    if open_page:
+
+        def target():
+            time.sleep(open_delay)
+            cfg = s.cfg.web_server
+            url = f'http://{cfg.host}:{cfg.port}'
+
+            webbrowser.open(url, new=1, autoraise=True)
+
+        Thread(target=target, daemon=True).start()
+    s.run(db)

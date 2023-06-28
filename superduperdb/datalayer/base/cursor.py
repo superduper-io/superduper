c406450cd2db8c6aa13cb0d181ccec7ebd331396
Tom Ritchford <tom@swirly.com>
2023-06-28 15:05:45 +0200

    Turn SuperDuperCursor into a dataclass

--- superduperdb/datalayer/base/cursor.py
+++ superduperdb/datalayer/base/cursor.py
@@ -1,39 +1,28 @@
+import dataclasses as dc
 import typing as t
-
-from superduperdb.core.encoder import Encoder
+from functools import cached_property
+from pymongo.cursor import Cursor
 from superduperdb.core.documents import Document
+from superduperdb.core.encoder import Encoder
 from superduperdb.misc.special_dicts import MongoStyleDict
 
 
+@dc.dataclass
 class SuperDuperCursor:
-    _results: t.List[float]
+    raw_cursor: Cursor
+    id_field: str
+    types: t.Dict[str, Encoder] = dc.field(default_factory=dict)
+    features: t.Optional[t.Dict[str, str]] = None
+    scores: t.Optional[t.List[float]] = None
+    _it: int = 0
 
-    def __init__(
-        self,
-        cursor,
-        id_field: str,
-        types: t.Mapping[str, Encoder],
-        features: t.Union[t.Mapping[str, str], None] = None,
-        scores: t.Optional[t.List[float]] = None,
-    ):
-        self.cur = cursor
-        self.features = features
-        self.scores = scores
-        self.types = types
-        self.id_field = id_field
+    @cached_property
+    def _results(self) -> t.Optional[t.List[t.Dict]]:
+        def key(r):
+            # TODO: this is an actual error, because self.scores is a list
+            return -self.scores[str(r[self.id_field])]  # type: ignore
 
-        if self.scores is not None:
-            self._results = []
-            while True:
-                try:
-                    self._results.append(self.cur.next())
-                except StopIteration:
-                    break
-            self._results = sorted(
-                self._results,
-                key=lambda r: -self.scores[str(r[self.id_field])],  # type: ignore
-            )
-            self.it = 0
+        return None if self.scores is None else sorted(self.raw_cursor, key=key)
 
     def _add_features(self, r):
         r = MongoStyleDict(r)
@@ -51,12 +40,12 @@ class SuperDuperCursor:
     def __next__(self):
         if self.scores is not None:
             try:
-                r = self._results[self.it]
+                r = self._results[self._it]
             except IndexError:
                 raise StopIteration
-            self.it += 1
+            self._it += 1
         else:
-            r = self.cur.next()
+            r = self.raw_cursor.next()
         if self.scores is not None:
             r['_score'] = self.scores[str(r[self.id_field])]
         if self.features is not None and self.features:

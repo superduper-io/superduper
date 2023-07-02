823d294e410352b99296782e6f778a253760adb2
Tom Ritchford <tom@swirly.com>
2023-07-02 14:01:03 +0200

    Teach caches to expire

--- tests/unittests/misc/test_typed_cache.py
+++ tests/unittests/misc/test_typed_cache.py
@@ -1,11 +1,31 @@
 from superduperdb.misc.typed_cache import TypedCache
+from unittest import mock
 
 
 def test_typed_cache():
+    now = 0
+
+    def time():
+        nonlocal now
+        now += 1
+        return now - 1
+
     cache = TypedCache()
 
-    assert cache.put(23) == 'int-0'
-    assert cache.put('123') == 'str-0'
+    with mock.patch('time.time', side_effect=time):
+        assert cache.put(23) == 'int-0'
+        assert cache.put('123') == 'str-0'
 
-    assert cache.get('int-0') == 23
-    assert cache.get('str-0') == '123'
+        assert cache.get('int-0') == 23
+        assert cache.get('str-0') == '123'
+
+        assert cache.put(32) == 'int-1'
+        assert cache.put(235) == 'int-2'
+
+        assert len(cache) == 4
+
+        removed = cache.expire(3)
+        assert len(cache) == 1
+
+        expected = {int: {'0': 23, '1': 32}, str: {'0': '123'}}
+        assert removed == expected

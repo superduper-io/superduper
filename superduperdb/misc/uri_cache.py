69dabdb7f11f293f1b1bb0a42b5133fd735d94aa
Tom Ritchford <tom@swirly.com>
2023-07-02 12:47:34 +0200

    Add misc/uri_cache.py

---
+++ superduperdb/misc/uri_cache.py
@@ -0,0 +1,56 @@
+from .for_each import for_each
+from .typed_cache import TypedCache
+import typing as t
+
+if t.TYPE_CHECKING:
+    import dataclasses as dc
+else:
+    from . import dataclasses as dc
+
+
+ContentType = t.TypeVar('ContentType')
+
+
+@dc.dataclass
+class Cached(t.Generic[ContentType]):
+    """A base class for classes that are cached using their uri and type"""
+
+    _content: dc.InitVar[ContentType] = None
+    uri: str = ''
+
+    @property
+    def content(self) -> ContentType:
+        """Get the cached content. `content` is not JSONized"""
+        return self._content  # type: ignore[return-value, attr-defined]
+
+    @content.setter
+    def content(self, content: ContentType):
+        """Set the cached content. `content` is not JSONized"""
+        self._content = content  # type: ignore[return-value, attr-defined]
+
+    def __post_init__(self, content: ContentType):
+        self.content = content
+
+
+class URICache(TypedCache):
+    """
+    A typed cache for instances of `Cached`.
+    """
+
+    def cache(self, entry):
+        """Add a uri to the content in an instance of Cached"""
+        if isinstance(entry, Cached):
+            entry.uri = self.put(entry.content)
+
+    def uncache(self, entry: t.Any) -> None:
+        """Retrieve the content from the uri in an instance of Cached"""
+        if isinstance(entry, Cached):
+            entry.content = self.get(entry.uri)
+
+    def cache_all(self, entry: t.Any) -> None:
+        """Run self.cache recursively"""
+        for_each(self.cache, entry)
+
+    def uncache_all(self, entry: t.Any) -> None:
+        """Run self.uncache recursively"""
+        for_each(self.uncache, entry)

969fe352183364f2bb4eb986b29fdaf9898d0c17
Tom Ritchford <tom@swirly.com>
2023-07-10 13:44:34 +0200

    Clean superduperdb/__init__.py
    Former-commit-id: d3e9e21fdd63cac663b29b6279697be18b661821

--- superduperdb/__init__.py
+++ superduperdb/__init__.py
@@ -2,13 +2,12 @@ from .misc import config, configs
 from .misc.jsonable import JSONable
 from pathlib import Path
 
-__all__ = 'CFG', 'ICON', 'JSONable', 'ROOT', 'config', 'log'
+__all__ = 'CFG', 'ICON', 'JSONable', 'ROOT', 'config', 'log', 'logging'
 
 CFG = configs.build_config()
 ICON = '🔮'
 ROOT = Path(__file__).parent
 
 from .misc import logger  # noqa: E402
-from superduperdb.misc.superduper import superduper  # noqa: E402,F401
 
 log = logger.logging

a25c2feb7a2efda7ea79724df70950d6f0759416
Tom Ritchford <tom@swirly.com>
2023-07-11 14:58:07 +0200

    Add typing, docs to misc/serialization.py
    * Identify issues in misc/serialization.py and datalayer/mongodb/cdc.py
    * Fix an unassigned member variable in models/torch/wrapper.py
    * Remove unused code from datalayer/base/artifacts.py

--- superduperdb/misc/serialization.py
+++ superduperdb/misc/serialization.py
@@ -1,67 +1,80 @@
 import importlib
-import inspect
 import io
 import pickle
 from abc import ABC
-
 import dill
 import torch
+import typing as t
+import typing_extensions as te
+
+Info = t.Optional[t.Dict[str, t.Any]]
 
 
-def to_dict(item):
-    return {
-        'module': item.__class__.__module__,
-        'cls': item.__class__.__name__,
-        'dict': item.dict(),
-    }
+class ModuleClassDict(te.TypedDict):
+    """A ``dict``with the module, class and JSONization of an object"""
+
+    module: str
+    cls: str
+    dict: t.Dict[str, t.Any]
 
 
-def from_dict(r, db=None):
-    module = importlib.import_module(f'{r["module"]}')
+def to_dict(item: t.Any) -> ModuleClassDict:
+    """Convert an item into a ``ModuleClassDict``
+
+    :param item
+        The item to be converted
+    """
+    return ModuleClassDict(
+        module=item.__class__.__module__,
+        cls=item.__class__.__name__,
+        dict=item.dict(),
+    )
+
+
+def from_dict(r: ModuleClassDict, **kwargs) -> t.Any:
+    """Construct an item from a ``ModuleClassDict``
+
+    :param r
+        A ModuleClassDict with the module, class and JSONization of an object
+    """
+    module = importlib.import_module(r['module'])
     cls = getattr(module, r['cls'])
-    if 'db' in inspect.signature(cls.__init__).parameters:
-        return cls(**r['dict'], db=db)
-    else:
-        return cls(**r['dict'])
+    return cls(**r['dict'], **kwargs)
 
 
-class _BaseSerializer(ABC):
+class Serializer(ABC):
     @staticmethod
-    def encode(self, object):
+    def encode(object: t.Any, info: Info = None) -> bytes:
         raise NotImplementedError
 
     @staticmethod
-    def decode(self, bytes):
+    def decode(b: bytes, info: Info = None) -> t.Any:
         raise NotImplementedError
 
 
-class PickleSerializer:
+class PickleSerializer(Serializer):
     @staticmethod
-    def encode(object):
-        f = io.BytesIO()
-        pickle.dump(object, f)
-        return f.getvalue()
+    def encode(object: t.Any, info: Info = None) -> bytes:
+        return pickle.dumps(object)
 
     @staticmethod
-    def decode(bytes):
-        return pickle.load(io.BytesIO(bytes))
+    def decode(b: bytes, info: Info = None) -> t.Any:
+        return pickle.loads(b)
 
 
-class DillSerializer:
+class DillSerializer(Serializer):
     @staticmethod
-    def encode(object):
-        f = io.BytesIO()
-        dill.dump(object, f)
-        return f.getvalue()
+    def encode(object: t.Any, info: Info = None) -> bytes:
+        return dill.dumps(object)
 
     @staticmethod
-    def decode(bytes):
-        return dill.load(io.BytesIO(bytes))
+    def decode(b: bytes, info: Info = None) -> t.Any:
+        return dill.loads(b)
 
 
-class TorchSerializer:
+class TorchSerializer(Serializer):
     @staticmethod
-    def encode(object):
+    def encode(object: t.Any, info: Info = None) -> bytes:
         was_gpu = object.device == 'cuda'
         object.to('cpu')
         f = io.BytesIO()
@@ -71,25 +84,26 @@ class TorchSerializer:
         return f.getvalue()
 
     @staticmethod
-    def decode(bytes):
-        return torch.load(io.BytesIO(bytes))
+    def decode(b: bytes, info: Info = None) -> t.Any:
+        return torch.load(io.BytesIO(b))
 
 
-class TorchStateSerializer:
+class TorchStateSerializer(Serializer):
     @staticmethod
-    def encode(object, info):
+    def encode(object: t.Any, info: Info = None) -> bytes:
         f = io.BytesIO()
         torch.save(object.state_dict(), f)
         return f.getvalue()
 
     @staticmethod
-    def decode(object, info):
-        instance = from_dict(info)
+    def decode(object: t.Any, info: Info = None) -> t.Any:
+        # BROKEN: decode's first argument is bytes, not t.Any!
+        instance = from_dict(info)  # type: ignore[arg-type]
         instance.load_state_dict(object)
         return instance
 
 
-serializers = {
+serializers: t.Dict[str, t.Type] = {
     'pickle': PickleSerializer,
     'dill': DillSerializer,
     'torch': TorchSerializer,

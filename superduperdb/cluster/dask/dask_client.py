62edca6d2170c8e6469b2aef53fc3c131937ef17
thejumpman2323 <kartik@superduperdb.com>
2023-07-13 00:35:58 +0530

    Add local cluster in dask

--- superduperdb/cluster/dask/dask_client.py
+++ superduperdb/cluster/dask/dask_client.py
@@ -1,24 +1,31 @@
 import uuid
 
+from dask.distributed import LocalCluster
 from dask.distributed import Client, wait, fire_and_forget
 
 from superduperdb.misc.logger import logging
 
 
 class DaskClient:
-    def __init__(self, address: str, serializers=None, deserializers=None):
+    def __init__(self, address: str, serializers=None, deserializers=None, local=False):
         self.futures_collection = {}
-        self.client = Client(
-            address=address,
-            serializers=serializers,
-            deserializers=deserializers,
-        )
+        if local:
+            cluster = LocalCluster(n_workers=1)
+            self.client = Client(cluster)
+        else:
+            self.client = Client(
+                address=address,
+                serializers=serializers,
+                deserializers=deserializers,
+            )
 
     def submit(self, function, **kwargs):
         future = self.client.submit(function, **kwargs)
         identifier = kwargs.get('identifier', None)
         if not identifier:
-            logging.warning('Could not get an identifier from submitted function, creating one!')
+            logging.warning(
+                'Could not get an identifier from submitted function, creating one!'
+            )
             identifier = str(uuid.uuid4())
         self.futures_collection[identifier] = future
         return future
@@ -28,8 +35,11 @@ class DaskClient:
         fire_and_forget(future)
         return future
 
-    def wait_all_pending_task(self):
-        futures = self.futures_collection.values()
+    def shutdown(self):
+        self.client.shutdown()
+
+    def wait_all_pending_tasks(self):
+        futures = list(self.futures_collection.values())
         wait(futures)
 
     def get_result(self, identifier: str):
@@ -37,9 +47,10 @@ class DaskClient:
         return self.client.gather(future)
 
 
-def dask_client(cfg):
+def dask_client(cfg, local=False):
     return DaskClient(
         address=f'tcp://{cfg.ip}:{cfg.port}',
         serializers=cfg.serializers,
         deserializers=cfg.deserializers,
+        local=local,
     )

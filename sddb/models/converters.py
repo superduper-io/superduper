26d033e9a1fd0cec3edb7a5c68ddc2eb695094fc
Duncan Blythe <duncan@superduperdb.com>
2023-07-13 14:35:38 -0400

    finished pickle refactor

--- sddb/models/converters.py
+++ sddb/models/converters.py
@@ -1,55 +1,16 @@
-import io
-import numpy
-import PIL.Image
-import torch
+from sddb.models import loading
 
-from sddb.utils import import_object
 
 converters = {}
 
 
 def encode(handle, bytes_):
     if handle not in converters:
-        converters[handle] = import_object(handle)
+        converters[handle] = loading.load({'path': handle})
     return converters[handle].encode(bytes_)
 
 
 def decode(handle, bytes_):
     if handle not in converters:
-        converters[handle] = import_object(handle)
+        converters[handle] = loading.load({'path': handle})
     return converters[handle].decode(bytes_)
-
-
-class Utf8:
-    @staticmethod
-    def encode(x):
-        return bytes(x)
-
-    @staticmethod
-    def decode(x):
-        return x.decode('utf-8')
-
-
-class PILImage:
-    @staticmethod
-    def encode(x):
-        buffer = io.BytesIO()
-        x.save(buffer, format='png')
-        return buffer.getvalue()
-
-    @staticmethod
-    def decode(bytes_):
-        return PIL.Image.open(io.BytesIO(bytes_))
-
-
-class FloatTensor:
-    @staticmethod
-    def encode(x):
-        x = x.numpy()
-        assert x.dtype == numpy.float32
-        return memoryview(x).tobytes()
-
-    @staticmethod
-    def decode(bytes_):
-        array = numpy.frombuffer(bytes_, dtype=numpy.float32)
-        return torch.from_numpy(array).type(torch.float)

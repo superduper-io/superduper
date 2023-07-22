d029d5b60bc1dc3100f2606d4ddd12fd1f9e38d2
Tom Ritchford <tom@swirly.com>
2023-07-22 12:14:16 +0200

    Simplify cluster/ray/predict.py
    Former-commit-id: b9c8cadfa6595ccd9c90800198a124a91c796f5e

--- superduperdb/cluster/ray/predict.py
+++ superduperdb/cluster/ray/predict.py
@@ -1,5 +1,4 @@
-from bson import BSON
-import click
+import bson
 from ray import serve as _serve
 from starlette.requests import Request
 
@@ -17,10 +16,9 @@ def create_server(model: str, num_replicas):
             self.db = build_datalayer()
             self.model = self.db.models[model]
 
-        async def __call__(self, http_request: Request) -> str:
+        async def __call__(self, http_request: Request) -> bytes:
             data = await http_request.body()
-            data = BSON.decode(data)  # type: ignore[arg-type]
-            print(data)
+            data = bson.decode(data)
             data = [Document.decode(r, encoders=self.db.encoders) for r in data]
             X = Document.decode(data['X'], encoders=self.db.encoders)
             try:
@@ -28,18 +26,6 @@ def create_server(model: str, num_replicas):
             except Exception:
                 X = [x.unpack() for x in X]
             result = self.model.predict(X)
-            return BSON.encode({'output': result.encode()})  # type: ignore
+            return bson.encode({'output': result.encode()})
 
     return Server
-
-
-@click.command()  # type: ignore[arg-type]
-@click.argument('model')
-@click.option('--num_replicas', default=1)
-def serve(model: str, num_replicas: int = 1):
-    Server = create_server(model, num_replicas=num_replicas)
-    Server.bind()
-
-
-if __name__ == '__main__':
-    serve()

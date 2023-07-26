28d4546252b845c313bc380520d999ac1a9eca7d
Tom Ritchford <tom@swirly.com>
2023-07-26 12:06:14 +0200

    Add edited MonkeyType annotations to core
    Former-commit-id: e457f86e56d5dac494519cc3b49c0c894b312e4d

--- superduperdb/core/task_workflow.py
+++ superduperdb/core/task_workflow.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import dataclasses as dc
 import typing as t
 
@@ -6,6 +7,11 @@ import networkx
 from superduperdb.misc.configs import CFG
 
 from .job import Job
+from distributed.client import Future
+from superduperdb.core.job import ComponentJob, FunctionJob
+
+if t.TYPE_CHECKING:
+    from superduperdb.datalayer.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -13,21 +19,22 @@ class TaskWorkflow:
     database: t.Any
     G: networkx.DiGraph = dc.field(default_factory=networkx.DiGraph)
 
-    def add_edge(self, node1, node2) -> None:
+    def add_edge(self, node1: str, node2: str) -> None:
         self.G.add_edge(node1, node2)
 
-    def add_node(self, node, job) -> None:
+    def add_node(self, node: str, job: t.Union[FunctionJob, ComponentJob]) -> None:
         self.G.add_node(node, job=job)
 
-    def dependencies(self, node):
+    def dependencies(self, node: str) -> t.List[t.Optional[t.Union[t.Any, Future]]]:
         return [self.G.nodes[a]['job'].future for a in self.G.predecessors(node)]
 
     def watch(self):
         for node in list(networkx.topological_sort(self.G)):
-            job: Job = self.G.nodes[node]['job']
-            job.watch()
+            self.G.nodes[node]['job'].watch()
 
-    def __call__(self, db=None, distributed: t.Optional[bool] = None):
+    def __call__(
+        self, db: t.Optional[Datalayer] = None, distributed: bool = False
+    ) -> 'TaskWorkflow':
         if distributed is None:
             distributed = CFG.distributed
 

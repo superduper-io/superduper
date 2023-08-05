08d21079b864544763a67ec0ca1ada7eedb7dc7f
thejumpman2323 <kartik@superduperdb.com>
2023-08-05 21:32:25 +0530

    Add outputs method in documents

--- test/unittest/component/test_documents.py
+++ test/unittest/component/test_documents.py
@@ -1,14 +1,22 @@
+import pytest
 import torch
 
 from superduperdb.container.document import Document
 from superduperdb.ext.torch.tensor import tensor
 
 
-def test_document_encoding():
+@pytest.fixture(scope='function')
+def document():
     t = tensor(torch.float, shape=(20,))
 
-    r = Document({'x': t(torch.randn(20))})
+    yield Document({'x': t(torch.randn(20)), '_outputs': {'x': {'model_test': 1}}})
 
-    print(r.encode())
 
-    Document.decode(r.encode(), encoders={'torch.float32[20]': t})
+def test_document_encoding(document):
+    t = tensor(torch.float, shape=(20,))
+    print(document.encode())
+    Document.decode(document.encode(), encoders={'torch.float32[20]': t})
+
+
+def test_document_outputs(document):
+    assert document.outputs('x', 'model_test') == 1

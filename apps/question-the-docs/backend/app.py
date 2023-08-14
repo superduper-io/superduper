4afbd9ba923188924094002eccc610abe5e706de
thejumpman2323 <kartik@superduperdb.com>
2023-08-14 16:39:46 +0530

    Add contextualize utils in qa app

--- apps/question-the-docs/backend/app.py
+++ apps/question-the-docs/backend/app.py
@@ -3,6 +3,8 @@ from backend.document.routes import document_router
 from fastapi import FastAPI
 from pymongo import MongoClient
 
+from .ai.bot import setup_qa_documentation
+
 
 def init_routers(app: FastAPI) -> None:
     app.include_router(document_router)
@@ -14,12 +16,16 @@ def create_app() -> FastAPI:
     @_app.on_event("startup")
     def startup_db_client():
         _app.mongodb_client = MongoClient(settings.DB_URL)
-        _app.mongodb = _app.mongodb_client[settings.DB_NAME]
+        _client = _app.mongodb_client[settings.DB_NAME]
+        _app.mongodb = _client
+
+        setup_qa_documentation(_client)
+
+
 
     @_app.on_event("shutdown")
     def shutdown_db_client():
         _app.mongodb_client.close()
 
     init_routers(app=_app)
-
     return _app

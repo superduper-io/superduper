d064b9ff1d7fb2ed733f301ee37a2c3d3d81dee3
Duncan Blythe <duncan@superduperdb.com>
2023-08-15 14:51:25 +0200

    Change structure of the ai directory in QTD

--- apps/question-the-docs/backend/document/routes.py
+++ apps/question-the-docs/backend/document/routes.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Body, Request
-from superduperdb import superduper
+from fastapi import APIRouter, Request
 from superduperdb.db.mongodb.query import Collection
 
 from backend.document.models import Query
@@ -8,23 +7,23 @@ from backend.config import settings
 document_router = APIRouter(prefix="/document", tags=["docs"])
 
 
-def concept_assist_prompt_build(famous_person):
-    return (
-        f'Use the following description and code-snippets aboout SuperDuperDB to answer this question about SuperDuperDB in the voice of {famous_person}\n'
-        'Do not use any other information you might have learned about other python packages\n'
-        'Only base your answer on the code-snippets retrieved\n'
-        '{context}\n\n'
-        'Here\'s the question:\n'
-    )
-
-
 @document_router.post(
     "/query",
     response_description="Query document database for data to answer prompt",
 )
-def query_docs(request: Request, query: Query = Body(...)):
-    db = superduper(request.app.mongodb_client.my_database_name)
-
-    context_select = Collection(name="markdown").like({"text": query.query}, n=settings.NEAREST_TO_QUERY, vector_index="documentation_index").find({})
+def query_docs(request: Request, query: Query) -> dict:
+    db = request.app.superduperdb
+    collection = Collection(name=settings.MONGO_COLLECTION_NAME)
+    # build your query here combining vector-search "like(...)"
+    # with classical mongodb queries "find(...)"
+    context_select = (
+        collection
+            .like(
+                {"text": query.query},
+                n=settings.nearest_to_query,
+                vector_index="documentation_index"
+            )
+            .find()
+    )
     response, _ = db.predict('gpt-3.5-turbo', input=query.query, context_select=context_select, context_key='text')
     return response.unpack()

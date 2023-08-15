8b9866862fb7e12d6dd103a30b9bba90d0a1b594
Duncan Blythe <duncan@superduperdb.com>
2023-08-15 14:51:25 +0200

    Change structure of the ai directory in QTD
    Former-commit-id: f68bd77ee809aea00992658148071a89bc493371

--- apps/question-the-docs/backend/app.py
+++ apps/question-the-docs/backend/app.py
@@ -1,8 +1,13 @@
+from apps.question-the-docs.backend.ai.components import setup_ai
+from apps.question-the-docs.backend.ai.data import setup_data
 from backend.config import settings
 from backend.document.routes import document_router
-from backend.ai.bot import setup_qa_documentation
+from backend.ai.data import setup_qa_documentation
 from fastapi import FastAPI
 from pymongo import MongoClient
+
+from superduperdb.misc import superduper
+from backend.ai.setup import setup as setup_ai_and_data
 
 
 def init_routers(app: FastAPI) -> None:
@@ -15,11 +20,11 @@ def create_app() -> FastAPI:
     @_app.on_event("startup")
     def startup_db_client():
         _app.mongodb_client = MongoClient(settings.DB_URL)
-        _app.mongodb = _app.mongodb_client[settings.DB_NAME]
+        _app.superduperdb = superduper(_app.mongodb_client[settings.MONGO_DB_NAME])
 
-        setup_qa_documentation(_app.mongodb_client)
-
-
+        _app.superduperdb.drop(force=True)
+        setup_data(_app.superduperdb)
+        setup_ai(_app.superduperdb)
 
     @_app.on_event("shutdown")
     def shutdown_db_client():

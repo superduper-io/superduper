cba5c01c2e17edda12a01f672b933c62f7669a74
Nick Byrne <byrnen8@tcd.ie>
2023-08-15 18:35:58 +0200

    Refactor QTD app

--- apps/question-the-docs/backend/app.py
+++ apps/question-the-docs/backend/app.py
@@ -1,30 +1,35 @@
-from apps.question-the-docs.backend.ai.components import setup_ai
-from apps.question-the-docs.backend.ai.data import setup_data
+from backend.ai.components import install_ai_components
+from backend.ai.artifacts import load_ai_artifacts
 from backend.config import settings
-from backend.document.routes import document_router
-from backend.ai.data import setup_qa_documentation
+from backend.documents.routes import documents_router
 from fastapi import FastAPI
 from pymongo import MongoClient
 
-from superduperdb.misc import superduper
-from backend.ai.setup import setup as setup_ai_and_data
+from superduperdb import superduper
 
 
 def init_routers(app: FastAPI) -> None:
-    app.include_router(document_router)
+    app.include_router(documents_router)
 
 
 def create_app() -> FastAPI:
-    _app = FastAPI(title="KPI Dashboards")
+    _app = FastAPI(title="Question the Docs")
 
     @_app.on_event("startup")
     def startup_db_client():
-        _app.mongodb_client = MongoClient(settings.DB_URL)
-        _app.superduperdb = superduper(_app.mongodb_client[settings.MONGO_DB_NAME])
+        _app.mongodb_client = MongoClient(settings.mongo_uri)
+        _app.mongodb = _app.mongodb_client[settings.mongo_db_name]
 
-        _app.superduperdb.drop(force=True)
-        setup_data(_app.superduperdb)
-        setup_ai(_app.superduperdb)
+        # We wrap our MongoDB to make it a SuperDuperDB!
+        _app.superduperdb = superduper(_app.mongodb)
+
+        # EXPLAIN ARTIFACTS HERE.
+        load_ai_artifacts(_app.superduperdb)
+
+        # We populate our SuperDuperDB with AI components.
+        # EXPLAIN COMPONENTS HERE.
+        # These will be used later to answer questions on our data.
+        install_ai_components(_app.superduperdb)
 
     @_app.on_event("shutdown")
     def shutdown_db_client():
---
+++ apps/question-the-docs/backend/documents/routes.py
@@ -0,0 +1,35 @@
+from backend.config import settings
+from backend.documents.models import Answer, Query
+from fastapi import APIRouter, Request
+
+from superduperdb.db.mongodb.query import Collection
+
+documents_router = APIRouter(prefix="/documents", tags=["docs"])
+
+
+@documents_router.post(
+    "/query",
+    response_description="Query document database for data to answer prompt",
+)
+async def query_docs(request: Request, query: Query) -> Answer:
+    # Step 1: Build your query
+    # Build your query here combining vector-search "like(...)"
+    # with classical mongodb queries "find(...)"
+    collection = Collection(name=settings.mongo_collection_name)
+    context_select = collection.like(
+        {settings.vector_embedding_key: query.query},
+        n=settings.nearest_to_query,
+        vector_index="documentation_index",
+    ).find()
+
+    # Step 2: Execute your query
+    # INSERT INFORMATION HERE
+    db = request.app.superduperdb
+    db_response, _ = await db.apredict(
+        'gpt-3.5-turbo',
+        input=query.query,
+        context_select=context_select,
+        context_key=settings.vector_embedding_key,
+    )
+
+    return Answer(answer=db_response.unpack())

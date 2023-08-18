4df0c4823c89aeb26bbcc7b938abb6f53f045151
thejumpman2323 <kartik@superduperdb.com>
2023-08-18 15:34:35 +0530

    Add prelike and postlike

--- superduperdb/db/ibis/cursor.py
+++ superduperdb/db/ibis/cursor.py
@@ -1,8 +1,13 @@
 import dataclasses as dc
+import typing as t
+
 from superduperdb.db.base.cursor import SuperDuperCursor
+
 
 @dc.dataclass
 class SuperDuperIbisCursor(SuperDuperCursor):
+    db: t.Any = None
+
     def execute(self):
         raw_cursor = self.raw_cursor.execute()
         self.dict_cursor = raw_cursor.to_dict(orient="records")
@@ -11,9 +16,9 @@ class SuperDuperIbisCursor(SuperDuperCursor):
         return self
 
     def cursor_next(self):
-       if self._index < self._n:
-           row = self.dict_cursor[self._index]
-           self._index += 1
-           return row
-       else:
-           raise StopIteration
+        if self._index < self._n:
+            row = self.dict_cursor[self._index]
+            self._index += 1
+            return row
+        else:
+            raise StopIteration

c9ae6be1e8553e17b97984458faddaecf498afa0
Jay Chung <zhongjiajie955@gmail.com>
2023-08-27 18:51:57 +0800

    add combine-as-imports = true for ruff.isort

--- superduperdb/db/base/build.py
+++ superduperdb/db/base/build.py
@@ -3,11 +3,11 @@ import inspect
 import superduperdb as s
 from superduperdb.db.base.backends import (
     artifact_stores,
+    connections as default_connections,
     data_backends,
     metadata_stores,
     vector_database_stores,
 )
-from superduperdb.db.base.backends import connections as default_connections
 from superduperdb.db.base.db import DB
 from superduperdb.server.dask_client import dask_client
 
--- test/unittest/conftest.py
+++ test/unittest/conftest.py
@@ -24,8 +24,7 @@ except ImportError:
 from pymongo import MongoClient
 from tenacity import RetryError, Retrying, stop_after_delay
 
-from superduperdb.base.config import DataLayer, DataLayers
-from superduperdb.base.config import MongoDB as MongoDBConfig
+from superduperdb.base.config import DataLayer, DataLayers, MongoDB as MongoDBConfig
 from superduperdb.container.dataset import Dataset
 from superduperdb.container.document import Document
 from superduperdb.container.listener import Listener

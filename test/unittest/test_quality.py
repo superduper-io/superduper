2f9ecf6ae3cfcdea285ff37e7bbddd366c08f4fc
Tom Ritchford <tom@swirly.com>
2023-08-27 12:54:58 +0200

    Add test/unittest/test_quality.py (fix #778)

---
+++ test/unittest/test_quality.py
@@ -0,0 +1,27 @@
+import re
+
+import superduperdb as s
+
+CODE_ROOTS = s.ROOT / 'superduperdb', s.ROOT / 'test'
+
+# DEFECTS maps defect names to functions that match a defect in a line of code.
+# Each pattern matches its own definition :-D so 1 is the lowest possible defect count.
+DEFECTS = {
+    'noqa': re.compile(r'# .*noqa: ').search,
+    'type_ignore': re.compile(r'# .*type: ignore').search,
+}
+
+# ALLOWABLE_DEFECTS has the allowable defect counts, which should be non-increasing
+# over time.
+ALLOWABLE_DEFECTS = {
+    'noqa': 14,
+    'type_ignore': 158,
+}
+
+
+def test_quality():
+    files = (f for root in CODE_ROOTS for f in sorted(root.glob('**/*.py')))
+    lines = [line for f in files for line in f.read_text().splitlines()]
+    defects = {k: sum(bool(v(line)) for line in lines) for k, v in DEFECTS.items()}
+
+    assert defects == ALLOWABLE_DEFECTS

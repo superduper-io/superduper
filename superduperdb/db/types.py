ffd2f820f47d1f09da3363f88851c429389e79ce
thejumpman2323 <kartik@superduperdb.com>
2023-08-28 20:42:45 +0530

    Add superduperdb types
    Former-commit-id: f80d051caec1d74a0e12fa9397d0bc88774299bc

---
+++ superduperdb/db/types.py
@@ -0,0 +1,3 @@
+int32 = 'int32'
+float32 = 'float32'
+string = 'string'
--- test/integration/test_ibis.py
+++ test/integration/test_ibis.py
@@ -1,13 +1,12 @@
+import os
 import ibis
 import PIL.Image
-import pandas
-from sklearn import svm
-#import torchvision
+import torchvision
 
-from superduperdb import superduper
+import pytest
+
 from superduperdb import CFG
-from superduperdb.container.encoder import Encoder
-from superduperdb.container.schema import Schema
+from superduperdb.db import types
 from superduperdb.db.base.build import build_vector_database
 from superduperdb.container.document import Document as D
 from superduperdb.db.ibis.db import IbisDB
@@ -18,123 +17,88 @@ from superduperdb.db.sqlalchemy.metadata import SQLAlchemyMetadata
 from superduperdb.db.ibis.query import Table
 from superduperdb.ext.pillow.image import pil_image
 from superduperdb.ext.torch.model import TorchModel
-# from superduperdb.container.schema import Schema
+
+@pytest.fixture(scope='session')
+def ibis_db():
+    connection = ibis.sqlite.connect("mydb.sqlite")
+
+    # Create data layer
+    db = IbisDB(
+        databackend=IbisDataBackend(conn=connection, name='ibis'),
+        metadata=SQLAlchemyMetadata(conn=connection.con, name='ibis'),
+        artifact_store=FileSystemArtifactStore(
+            conn='./.tmp', name='ibis'
+        ),
+        vector_database=build_vector_database(CFG.vector_search.type),
+    )
+    yield db
+    os.remove('mydb.sqlite')
 
 
-import os
-os.remove('mydb.sqlite')
-connection = ibis.sqlite.connect("mydb.sqlite")
+def test_end2end(ibis_db):
+    db = ibis_db
+    schema = IbisSchema(
+        identifier='my_table',
+        fields={
+            'id': 'int64',
+            'health': 'int32',
+            'age': 'int32',
+            'image': pil_image,
+        }
+    )
+    im = PIL.Image.open('test/material/data/test-image.jpeg')
+    data_to_insert = [
+        {'id': 1, 'health': 0, 'age': 25, 'image': im},
+        {'id': 2, 'health': 0, 'age': 25, 'image': im},
+        {'id': 3, 'health': 0, 'age': 25, 'image': im},
+        {'id': 4, 'health': 0, 'age': 25, 'image': im},
+    ]
+    t = Table(identifier='my_table', schema=schema)
+    t.create(db)
 
-# Insert some sample data into the table
-schema = ibis.schema({'id': 'int64', 'health': 'int32', 'age': 'int32'})
-# Create the table
-table_name = 'my_table'
-
-# connection.create_table(table_name, schema=schema)
-
-
-# create data layer
-db = IbisDB(
-    databackend=IbisDataBackend(conn=connection, name='ibis'),
-    metadata=SQLAlchemyMetadata(conn=connection.con, name='ibis'),
-    artifact_store=FileSystemArtifactStore(
-        conn='./.tmp', name='ibis'
-    ),
-    vector_database=build_vector_database(CFG.vector_search.type),
-)
-
-schema = IbisSchema(
-    identifier='my_table',
-    fields={
-        'id': 'int64',
-        'health': 'int32',
-        'age': 'int32',
-        'image': pil_image,
-    }
-)
-im = PIL.Image.open('test/material/data/test-image.jpeg')
-data_to_insert = [
-    {'id': 1, 'health': 0, 'age': 25, 'image': im},
-    {'id': 2, 'health': 0, 'age': 25, 'image': im},
-    {'id': 3, 'health': 0, 'age': 25, 'image': im},
-    {'id': 4, 'health': 0, 'age': 25, 'image': im},
-]
-t = Table(identifier='my_table', schema=schema)
-t.create(db)
-im_b = open('test/material/data/test-image.jpeg', 'rb').read()
-db.add(t)
-db.execute(
-    t.insert([D({'id': d['id'], 'health': d['health'], 'age': d['age'], 'image': d['image']}) for d in data_to_insert])
-)
+    db.add(t)
+    db.execute(
+        t.insert([D({'id': d['id'], 'health': d['health'], 'age': d['age'], 'image': d['image']}) for d in data_to_insert])
+    )
 
 
 
-# -------------- retrieve data  from table ----------------
-imgs = db.execute(t.select("image", "age", "health"))
-for img in imgs:
-    print(img)
+    # -------------- retrieve data  from table ----------------
+    imgs = db.execute(t.select("image", "age", "health"))
+    for img in imgs:
+        print(img)
 
 
-import torchvision
+    # preprocessing function
+    preprocess = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(256),
+        torchvision.transforms.CenterCrop(224),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )])
+
+    def postprocess(x):
+        return int(x.topk(1)[1].item())
+
+    # create a torchvision model
+    resnet = TorchModel(
+            identifier='resnet18',
+        preprocess=preprocess,
+        postprocess=postprocess,
+
+        object=torchvision.models.resnet18(pretrained=False),
+        output_type=types.int32,
+    )
+
+    # Apply the torchvision model
+    resnet.predict(X='image', db=db, select=t.select('id', 'image'), max_chunk_size=3000, overwrite=True)
 
 
-# preprocessing function
-preprocess = torchvision.transforms.Compose([
-    torchvision.transforms.Resize(256),
-    torchvision.transforms.CenterCrop(224),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize(
-    mean=[0.485, 0.456, 0.406],
-    std=[0.229, 0.224, 0.225]
-)])
+    # Query the results back
+    q = t.filter(t.age == 25).outputs('resnet18', db)
+    curr = db.execute(q)
 
-def postprocess(x):
-    return int(x.topk(1)[1].item())
-
-# create a torchvision model
-resnet = TorchModel(
-        identifier='resnet18',
-    preprocess=preprocess,
-    postprocess=postprocess,
-
-    object=torchvision.models.resnet18(pretrained=False),
-)
-
-# apply the torchvision model
-resnet.predict(X='image', db=db, select=t.select('id', 'image'), max_chunk_size=3000, overwrite=True)
-
-
-
-# Query the results back
-q = t.filter(t.age == 25).outputs('resnet18', db)
-curr = db.execute(q)
-for c in curr:
-    print(c)
-
-
-
-
-breakpoint()
-
-
-
-# ------------------ SKLEARN ------------------
-# add a sklearn mode
-svc = svm.SVC(gamma='scale', class_weight='balanced', C=100, verbose=True)
-svc.fit(pandas.DataFrame(data_to_insert).iloc[:, :3], [0, 1, 1, 1, 0])
-
-model = superduper(
-    svc,
-    postprocess=lambda x: int(x),
-    preprocess=lambda x: list(x.values()
-))
-
-model.predict(X='_base', db=db, select=t.filter(t.age > 25), max_chunk_size=3000, overwrite=True)    
-
-#  Query result back
-out_table = connection.table(model.identifier)
-q = t.filter(t.age > 26).outputs(model.identifier, db)
-
-curr = db.execute(q)
-for c in curr:
-    print(c)
+    for c in curr:
+        assert all([ k in ['id', 'health', 'age', 'image', 'output', 'query_id', 'key', 'input_id'] for k in c.unpack().keys()])

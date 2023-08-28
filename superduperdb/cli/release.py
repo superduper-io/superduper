2de27b3165d04cf7ae5eaa600d901fd29873a18b
Tom Ritchford <tom@swirly.com>
2023-08-28 12:29:54 +0200

    Add command `sddb release`
    Former-commit-id: ad77b9051fc0dd246c7cf79c12aee6eed6e7fa55

---
+++ superduperdb/cli/release.py
@@ -0,0 +1,30 @@
+import enum
+
+import safer
+from semver import Version
+
+import superduperdb as s
+from superduperdb.misc import run
+
+from . import command
+
+INIT = 'superduperdb/__init__.py'
+
+
+class Release(str, enum.Enum):
+    major = 'major'
+    minor = 'minor'
+    patch = 'patch'
+
+
+@command(help='Create a release commit')
+def release(update: Release):
+    old = Version.parse(s.__version__)
+    new = getattr(old, 'bump_' + update)()
+    print('Updating', old, 'to', new)
+    with open(INIT) as fp_in, safer.open(INIT, 'w') as fp_out:
+        for line in fp_in:
+            if line.startswith('__version__'):
+                line = f"__version__ = '{new}'\n"
+            fp_out.write(line)
+    run.run(('git', 'commit', INIT, '-m', f'Update to version v{new}'))

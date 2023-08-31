3d3ca4c2f52e25740246ae5665ab949dd67fbc0f
thejumpman2323 <kartik@superduperdb.com>
2023-08-31 17:36:36 +0530

    Add docstring in ibis/query

--- superduperdb/db/ibis/query.py
+++ superduperdb/db/ibis/query.py
@@ -16,21 +16,182 @@ from superduperdb.db.ibis.schema import IbisSchema
 
 PRIMARY_ID: str = 'id'
 
+IbisTableType = t.TypeVar('IbisTableType')
+ParentType = t.TypeVar('ParentType')
+
 
 class QueryType(enum.Enum):
     QUERY = 'query'
     ATTR = 'attr'
 
 
+@dc.dataclass
+class Table(Component):
+    '''
+    This is a representation of a table in ibis.
+    '''
+
+    # The name of the table
+    identifier: str
+    # The schema of the table
+    schema: t.Optional[IbisSchema] = None
+    # The table object
+    table: t.Any = None
+    # Primary id of the table
+    primary_id: str = 'id'
+
+    #: A unique name for the class
+    type_id: t.ClassVar[str] = 'table'
+    version: t.ClassVar[t.Optional[int]] = None
+
+    def on_create(self, db: DB):
+        self.create(db)
+
+    def create(self, db: DB):
+        assert self.schema is not None, "Schema must be set"
+        for e in self.schema.encoders:
+            db.add(e)
+        try:
+            db.db.create_table(self.identifier, schema=self.schema.map())
+        except Exception as e:
+            if 'already exists' in str(e):
+                pass
+            else:
+                raise e
+
+    def get_table(self, conn: t.Any) -> t.Any:
+        if self.table is None:
+            self.table = conn.table(self.identifier)
+        return self.table
+
+    @property
+    def name(self) -> str:
+        return self.identifier
+
+    def mutate_args(self, args: t.Sequence) -> t.Sequence:
+        mutated_args = []
+        assert self.schema is not None, "Schema must be set"
+        for attr in args:
+            if isinstance(attr, str):
+                if attr in self.schema.fields:
+                    mutated_args.append(self.schema.mutate_column(attr))
+                else:
+                    mutated_args.append(attr)
+            elif isinstance(attr, QueryLinker):
+                attr_query = attr.get_latest_query()
+                if attr_query.type == QueryType.ATTR.value:
+                    mutated_args.append(self.schema.mutate_column(attr.query_type))
+                else:
+                    mutated_args.append(attr)
+            else:
+                mutated_args.append(attr)
+
+        return mutated_args
+
+    def __getattr__(self, k: str) -> 'QueryLinker':
+        '''
+        This method is responsible for dynamically creating a query chain,
+        which can be executed on a database. This is done by creating a
+        QueryLinker object, which is a representation of a query chain.
+        Under the hood, this is done by creating a QueryChain object, which
+        is a representation of a query chain.
+        '''
+
+        if k in self.__dict__:
+            return self.__getattr__(k)
+
+        assert self.schema is not None, "Schema must be set"
+        if not (hasattr(IbisTable, k) or k in self.schema.fields):
+            raise AttributeError(k)
+
+        return QueryLinker(
+            self, query_type=k, members=QueryChain(k, type=QueryType.ATTR.value)
+        )
+
+    def like(self, r: t.Any = None, n: int = 10, vector_index: t.Optional[str] = None):
+        '''
+        This appends a query to the query chain where the query is repsonsible
+        for performing a vector search on the parent query chain inputs.
+
+        :param r: The vector to search for
+        :param n: The number of results to return
+        :param vector_index: The vector index to use
+        '''
+        k = 'prelike'
+        kwargs = {'r': r, 'n': n, 'vector_index': vector_index}
+        query = Query(k, args=[], sddb_kwargs=kwargs)
+        return QueryLinker(self, query_type=k, members=QueryChain(query))
+
+    def insert(
+        self,
+        *args,
+        refresh: bool = True,
+        verbose: bool = True,
+        encoders: t.Sequence = [],
+        valid_prob: float = 0.05,
+        **kwargs,
+    ):
+        '''
+        This appends a query to the query chain where the query is repsonsible
+        for inserting data into the table.
+
+        :param args: The data to insert
+        :param refresh: Whether to refresh the table after inserting
+        :param verbose: Whether to print the progress of the insert
+        :param encoders: The encoders to use
+        :param valid_prob: The probability of validating the data
+        '''
+        sddb_kwargs = {
+            'refresh': refresh,
+            'verbose': verbose,
+            'encoders': encoders,
+            'kwargs': {'valid_prob': valid_prob},
+            'documents': args[0],
+        }
+        kwargs.update({'table_name': self.identifier})
+        args = args[1:]
+
+        insert = Query(
+            'insert',
+            type=QueryType.QUERY.value,
+            args=t.cast(t.Sequence, args),
+            kwargs=kwargs,
+            sddb_kwargs=sddb_kwargs,
+            connection_parent=True,
+        )
+
+        qc = QueryChain(insert)
+        return QueryLinker(self, query_type='insert', members=qc)
+
+
 class Query:
+    '''
+    This is a representation of a single query object in ibis query chain.
+    This is used to build a query chain that can be executed on a database.
+    Query will be executed in the order they are added to the chain.
+
+    If we have a query chain like this:
+        query = t.select(['id', 'name']).limit(10)
+    here we have 2 query objects, `select` and `limit`.
+
+    `select` will be wrapped with this class and added to the chain.
+
+    :param name: The name of the query
+    :param type: The type of the query, either `query` or `attr`
+    :param args: The arguments to pass to the query
+    :param kwargs: The keyword arguments to pass to the query
+    :param sddb_kwargs: The keyword arguments from sddb to pass to the query
+    :param connection_parent: If True, the parent of the query will be the connection
+    '''
+
     def __init__(
         self,
-        name,
-        type=QueryType.QUERY.value,
-        args=[],
-        kwargs={},
-        sddb_kwargs={},
-        connection_parent=False,
+        name: str,
+        type: str = QueryType.QUERY.value,
+        args: t.Sequence = [],
+        kwargs: t.Dict = {},
+        sddb_kwargs: t.Dict = {},
+        connection_parent: bool = False,
     ):
         self.name = name
         query = query_lookup.get(name, None)
@@ -43,7 +204,13 @@ class Query:
         self.kwargs = kwargs
         self.connection_parent = connection_parent
 
-    def execute(self, db, parent, table, ibis_table=None):
+    def execute(
+        self,
+        db: DB,
+        parent: ParentType,
+        table: Table,
+        ibis_table: t.Optional[IbisTableType] = None,
+    ):
         if self.type == QueryType.ATTR.value:
             return getattr(parent, self.query.name)
 
@@ -69,7 +236,20 @@ class Query:
 
 
 class QueryChain:
-    def __init__(self, seed=None, type=QueryType.QUERY.value):
+    '''
+    This is a representation of a query chain. This is used to build a query chain
+    that can be executed on a database. Query will be executed in the order they are
+    added to the chain.
+
+    :param seed: The seed to start the chain with
+    :param type: The type of the seed, either `query` or `attr`
+    '''
+
+    def __init__(
+        self,
+        seed: t.Optional[t.Union[str, Query]] = None,
+        type: str = QueryType.QUERY.value,
+    ):
         if isinstance(seed, str):
             query = Query(seed, type=type)
         elif isinstance(seed, Query):
@@ -109,12 +289,23 @@ class QueryChain:
 
 @dc.dataclass
 class OutputTable:
+    '''
+    This is a representation of model output table in ibis
+    '''
+
+    # The name of the table
     model: str
+    # Primary id of the table
     primary_id: str = 'id'
+    # The table object
     table: t.Any = None
+    # The schema of the table
     output_type: t.Any = None
 
-    def create(self, conn):
+    def create(self, conn: t.Any):
+        '''
+        Create the table in the database
+        '''
         self.table = conn.create_table(self.model, schema=self.schema)
         return self.table
 
@@ -139,15 +330,30 @@ class OutputTable:
 
 @dc.dataclass
 class InMemoryTable(Component):
+    '''
+    This is a representation of a table in memory (memtable) in ibis.
+    '''
+
+    # The name of the table
     identifier: str
+    # The table object
     table: t.Any = None
+    # Primary id of the table
     primary_id: str = 'id'
+
     type_id: t.ClassVar[str] = 'inmemory_table'
 
     def mutate_args(self, args):
         return args
 
     def __getattr__(self, k):
+        '''
+        This method is responsible for dynamically creating a query chain,
+        which can be executed on a database. This is done by creating a
+        QueryLinker object, which is a representation of a query chain.
+        Under the hood, this is done by creating a QueryChain object, which
+        is a representation of a query chain.
+        '''
         if k in self.__dict__:
             return self.__getattr__(k)
 
@@ -159,110 +365,7 @@ class InMemoryTable(Component):
         )
 
 
-@dc.dataclass
-class Table(Component):
-    identifier: str
-    schema: t.Optional[IbisSchema] = None
-    table: t.Any = None
-    primary_id: str = 'id'
-    #: A unique name for the class
-    type_id: t.ClassVar[str] = 'table'
-    version: t.ClassVar[t.Optional[int]] = None
-
-    def on_create(self, db: DB):
-        self.create(db)
-
-    def create(self, db: DB):
-        assert self.schema is not None, "Schema must be set"
-        for e in self.schema.encoders:
-            db.add(e)
-        try:
-            db.db.create_table(self.identifier, schema=self.schema.map())
-        except Exception as e:
-            if 'already exists' in str(e):
-                pass
-            else:
-                raise e
-
-    def get_table(self, conn):
-        if self.table is None:
-            self.table = conn.table(self.identifier)
-        return self.table
-
-    @property
-    def name(self):
-        return self.identifier
-
-    def mutate_args(self, args):
-        mutated_args = []
-        for attr in args:
-            if isinstance(attr, str):
-                if attr in self.schema.fields:
-                    mutated_args.append(self.schema.mutate_column(attr))
-                else:
-                    mutated_args.append(attr)
-            elif isinstance(attr, QueryLinker):
-                attr_query = attr.get_latest_query()
-                if attr_query.type == QueryType.ATTR.value:
-                    mutated_args.append(self.schema.mutate_column(attr.query_type))
-                else:
-                    mutated_args.append(attr)
-            else:
-                mutated_args.append(attr)
-
-        return mutated_args
-
-    def __getattr__(self, k):
-        if k in self.__dict__:
-            return self.__getattr__(k)
-
-        if not (hasattr(IbisTable, k) or k in self.schema.fields):
-            breakpoint()
-            raise AttributeError(k)
-
-        return QueryLinker(
-            self, query_type=k, members=QueryChain(k, type=QueryType.ATTR.value)
-        )
-
-    def like(self, r=None, n=10, vector_index=None):
-        k = 'prelike'
-        kwargs = {'r': r, 'n': n, 'vector_index': vector_index}
-        query = Query(k, args=[], sddb_kwargs=kwargs)
-        return QueryLinker(self, query_type=k, members=QueryChain(query))
-
-    def insert(
-        self,
-        *args,
-        refresh: bool = True,
-        verbose: bool = True,
-        encoders: t.Sequence = [],
-        valid_prob: float = 0.05,
-        **kwargs,
-    ):
-        sddb_kwargs = {
-            'refresh': refresh,
-            'verbose': verbose,
-            'encoders': encoders,
-            'kwargs': {'valid_prob': valid_prob},
-            'documents': args[0],
-        }
-        kwargs.update({'table_name': self.identifier})
-        args = args[1:]
-
-        insert = Query(
-            'insert',
-            type=QueryType.QUERY.value,
-            args=args,
-            kwargs=kwargs,
-            sddb_kwargs=sddb_kwargs,
-            connection_parent=True,
-        )
-
-        qc = QueryChain(insert)
-        return QueryLinker(self, query_type='insert', members=qc)
-
-
-class LogicalExprMixin:
+class _LogicalExprMixin:
     def _logical_expr(self, other, members, collection, k):
         args = [other]
         members.append_query(Query(k, args=args, kwargs={}))
@@ -282,9 +385,22 @@ class LogicalExprMixin:
 
 
 @dc.dataclass
-class QueryLinker(Serializable, LogicalExprMixin):
+class QueryLinker(Serializable, _LogicalExprMixin):
+    '''
+    This class is responsible for linking together a query chain. It is
+    responsible for creating a query chain, which is a representation of a
+    ibis query. This is done by creating a QueryChain object, which creates
+    a list of `Query` objects. Each `Query` object is a representation of
+    a query in the query chain.
+    '''
+
+    # The table that this query chain is linked.
+    # This table is the parent ibis table.
     collection: Table
+
+    # The type of query that this query chain is linked.
     query_type: str = 'find'
+
     args: t.Sequence = dc.field(default_factory=list)
     kwargs: t.Dict = dc.field(default_factory=dict)
     members: QueryChain = dc.field(default_factory=QueryChain)
@@ -322,13 +438,22 @@ class QueryLinker(Serializable, LogicalExprMixin):
 
         return QueryLinker(self.collection, query_type='filter', members=members)
 
-    def build(self, db):
+    def build(self, db: DB):
         table = parent = self.collection.table
         for member in self.members:
             parent = member.execute(db, parent, self.collection, table)
         return parent
 
-    def outputs(self, model, db):
+    def outputs(self, model: str, db: DB):
+        '''
+        This method is responsible for returning the outputs of a model.
+        It is used to get the outputs of a model from a ibis query chain.
+        Example:
+            q = t.filter(t.age > 25).outputs('model_name', db)
+        The above query will return the outputs of the `model_name` model
+        with t.filter() ids.
+
+        '''
         curr_query = self.build(db)
         model_table = db.db.table(model)
         query = curr_query.join(
@@ -345,7 +470,7 @@ class QueryLinker(Serializable, LogicalExprMixin):
 
     def model_update(
         self,
-        db,
+        db: DB,
         ids: t.Sequence[t.Any],
         key: str,
         model: str,
@@ -373,6 +498,22 @@ class QueryLinker(Serializable, LogicalExprMixin):
         db.execute(Table(model).insert(table_record))
 
     def __call__(self, *args, **kwargs):
+        '''
+        This method is responsible to mutate the arguments of a query and
+        return a new QueryLinker object with updated members.
+        The last member of the query chain is updated with theses mutated arguments.
+        It also updates the query type of last member.
+
+        The mutation of arguments means if the query is `select` and args are,
+        `['name', 'age', 'image']`.
+        The above args contains a column `image` which is of type `pil_image`.
+        We can get the informantion from the `collection` schema fields.
+
+        because `image` is a `pil_image` type, it was stored in ibis table as
+        `image::encodable::pil_image`. So, we need to mutate the args to
+        `['name', 'age', 'image::encodable::pil_image']`.
+
+        '''
         args = self.collection.mutate_args(args)
 
         # TODO: handle kwargs
@@ -386,7 +527,7 @@ class QueryLinker(Serializable, LogicalExprMixin):
             kwargs=kwargs,
         )
 
-    def execute(self, db, parent, ibis_table):
+    def execute(self, db: DB, parent: t.Any, ibis_table: t.Any) -> t.Any:
         for member in self.members:
             parent = member.execute(db, parent, self.collection, ibis_table=ibis_table)
         return parent
@@ -464,7 +605,7 @@ class PreLike:
 
 @dc.dataclass
 class Insert:
-    documents: t.List['Document'] = dc.field(default_factory=list)
+    documents: t.Sequence['Document'] = dc.field(default_factory=list)
     refresh: bool = True
     verbose: bool = True
     kwargs: t.Dict = dc.field(default_factory=dict)

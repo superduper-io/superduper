788736c6e3d5b55be6318994c2114f3c3c26d894
Nick Byrne <byrnen8@tcd.ie>
2023-09-01 18:22:10 +0200

    Update CI to check imports

--- .github/ci-pinned-requirements/update-pinned-deps.sh
+++ .github/ci-pinned-requirements/update-pinned-deps.sh
@@ -4,9 +4,21 @@ set -e  # Stop execution if any command fails
 
 python -m pip install --upgrade pip-tools pip
 
+echo "Running pip-compile for core to produce pinned dependencies"
+python -m piptools compile \
+  -o .github/ci-pinned-requirements/core.txt \
+  --resolver backtracking \
+  --upgrade \
+  pyproject.toml
+
 deps=("docs" "dev")
 for dep in "${deps[@]}"
 do
     echo "Running pip-compile for $dep to produce pinned dependencies"
-    python -m piptools compile --extra "$dep" --upgrade --resolver backtracking -o .github/ci-pinned-requirements/"$dep".txt pyproject.toml
+    python -m piptools compile \
+    -o .github/ci-pinned-requirements/"$dep".txt \
+    --extra "$dep" \
+    --resolver backtracking \
+    --upgrade \
+    pyproject.toml
 done

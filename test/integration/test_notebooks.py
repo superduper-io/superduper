9329a0e3a124aea71360821a79289dfb726798c4
Tom Ritchford <tom@swirly.com>
2023-09-06 13:31:39 +0200

    Function-scope fixture in text_notebooks.py

--- test/integration/test_notebooks.py
+++ test/integration/test_notebooks.py
@@ -6,7 +6,7 @@ import tempfile
 import pytest
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def copy_superduperdb():
     with tempfile.TemporaryDirectory() as tmp_dir:
         shutil.copytree('superduperdb', os.path.join(tmp_dir, 'superduperdb'))

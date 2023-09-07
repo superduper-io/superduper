89037c2f783b08111bcdf9c9aa2c75aac6cc6e49
thejumpman2323 <kartik@superduperdb.com>
2023-09-07 15:41:04 +0530

    Add  huggingface in qtd

--- apps/question-the-docs/backend/ai/utils/github.py
+++ apps/question-the-docs/backend/ai/utils/github.py
@@ -18,6 +18,7 @@ class Repo:
     branch: str
     documentation_dir: str
     documentation_base_url: str
+    documentation_file_extension: str = 'html'
 
 
 superduperdb = Repo(
@@ -28,13 +29,17 @@ superduperdb = Repo(
     'https://superduperdb.github.io/superduperdb',
 )
 langchain = Repo(
-    'langchain-ai', 'langchain', 'master', 'docs/', 'https://python.langchain.com/docs'
-)
-fastchat = Repo(
-    'lm-sys', 'FastChat', 'main', 'docs/', 'https://lm-sys.github.io/FastChat'
+    'langchain-ai', 'langchain', 'master', 'docs/docs_skeleton/docs', 'https://python.langchain.com/docs'
 )
 
-REPOS = {'superduperdb': superduperdb, 'langchain': langchain, 'fastchat': fastchat}
+
+huggingface= Repo(
+    'huggingface', 'transformers', 'main', 'docs/source/en', 'https://huggingface.co/docs/transformers', documentation_file_extension=''
+)
+
+
+REPOS = {'superduperdb': superduperdb, 'langchain': langchain, 'huggingface': huggingface}
+#REPOS = {'langchain': langchain}
 
 
 # TODO: Use GraphQL API instead of REST API and convert to async
@@ -52,6 +57,7 @@ def gh_repo_contents(owner, repo, branch=None):
     if branch:
         return get_repo(branch)
     else:
+
         errs = []
         for br in ['main', 'master']:
             try:
@@ -93,16 +99,30 @@ def save_github_md_files_locally(repo):
     branch = repo_metadata.branch
     documentation_dir = repo_metadata.documentation_dir
     documentation_base_url = repo_metadata.documentation_base_url
+    doc_extension = repo_metadata.documentation_file_extension
 
     repo_contents = gh_repo_contents(owner, name, branch)
     urls = documentation_markdown_urls(repo_contents, documentation_dir)
 
-    Path(f'docs/{name}').mkdir(exist_ok=True, parents=True)
+    Path(f'docs/{repo}').mkdir(exist_ok=True, parents=True)
 
-    URL_CACHE[name] = urls
-    doc_base_path = (
-        lambda path, section: f'{documentation_base_url}/{path}.html#{section}'
-    )
+    URL_CACHE[(repo, '')] = documentation_base_url
+
+    def doc_base_path(path, section, ext):
+        if ext:
+            return f'{documentation_base_url}/{path}.{ext}#{section}'
+        else:
+            return f'{documentation_base_url}/{path}#{section}'
+
+
+    def _remove_overlap_prefix(x, y):
+        x_dirs = x.split('/')
+        y_dirs  = y.split('/')
+        for i in range(len(y_dirs)):
+            if y_dirs[i] == x_dirs[-i]:
+                return '/'.join(y_dirs[i+1:])
+        return y
+
 
     for i, url in enumerate(urls):
         content = download_and_decode(url['url'])
@@ -110,14 +130,13 @@ def save_github_md_files_locally(repo):
         for _, s in sections:
             relative_path = url['path']
             relative_path = '/'.join(relative_path.split('/')[1:])
+            relative_path = _remove_overlap_prefix(documentation_dir, relative_path)
             section_file = os.path.splitext(relative_path)[0]
             s_encoded = s.replace(' ', '-').lower()
             if documentation_base_url:
-                URL_CACHE[(name, s)] = doc_base_path(section_file, s_encoded)
+                URL_CACHE[(repo, s)] = doc_base_path(section_file, s_encoded, doc_extension)
             else:
-                URL_CACHE[(name, s)] = relative_path
-
-        with open(f'docs/{name}/file_{i}', 'wb') as f:
+                URL_CACHE[(repo, s)] = relative_path
+        with open(f'docs/{repo}/file_{i}', 'wb') as f:
             f.write(content)
-
-    return Path(f'docs/{name}').glob('*')
+    return Path(f'docs/{repo}').glob('*')

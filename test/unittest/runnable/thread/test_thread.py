0d1a64e4034d3d55cf9cf76e7acae184e00f4f82
Tom Ritchford <tom@swirly.com>
2023-09-10 15:06:03 +0200

    Add superduperdb/misc/runnable (see #883)
    Former-commit-id: 23fff8c1909cc9484b7d9540fe5b0e6d1cc20552

---
+++ test/unittest/runnable/thread/test_thread.py
@@ -0,0 +1,52 @@
+from superduperdb.misc import runnable
+
+
+def test_is_thread():
+    result = []
+
+    class IsThread(runnable.IsThread):
+        def callback(self):
+            result.append(0)
+            if len(result) >= 4:
+                self.stop()
+
+    it = IsThread()
+    assert not it.running
+    assert not it.stopped
+    assert not result
+
+    with it:
+        pass
+
+    assert it.stopped
+    assert result == [0]
+
+    result.clear()
+
+    it = IsThread()
+    it.looping = True
+
+    with it:
+        pass
+
+    assert it.stopped
+    assert result == [0, 0, 0, 0]
+
+
+def test_has_thread():
+    result = []
+
+    ht = runnable.HasThread()
+
+    def callback():
+        result.append(0)
+        if len(result) >= 4:
+            ht.stop()
+
+    ht.callback = callback
+
+    with ht:
+        pass
+
+    assert ht.stopped
+    assert result == [0]

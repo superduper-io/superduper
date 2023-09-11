ee84b60a8986c7ba7997a168601c54465c22060e
Tom Ritchford <tom@swirly.com>
2023-09-11 14:39:15 +0200

    Add apps to test_impall

--- apps/question-the-docs/backend/documents/models.py
+++ apps/question-the-docs/backend/documents/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic.v1 import BaseModel, Field
 
 
 class Repo(str):
--- test/unittest/test_impall.py
+++ test/unittest/test_impall.py
@@ -9,3 +9,4 @@ class ImpAllTest(impall.ImpAllTest):
     CLEAR_SYS_MODULES = False
     WARNINGS_ACTION = 'ignore'
     EXCLUDE = (torch is None) * ['superduperdb/ext/torch/**']
+    PATHS = 'superduperdb', 'test', 'apps/question-the-docs'

1753476c57823b2e542d0f8c8fa17871590de111
Tom Ritchford <tom@swirly.com>
2023-09-12 12:32:21 +0200

    Fix an import warning in runnable

--- superduperdb/misc/runnable/__init__.py
+++
@@ -1,5 +0,0 @@
-from .collection import HasRunnables, ThreadQueue
-from .runnable import Event, Runnable
-from .thread import HasThread, IsThread
-
-__all__ = 'Event', 'HasRunnables', 'HasThread', 'IsThread', 'Runnable', 'ThreadQueue'

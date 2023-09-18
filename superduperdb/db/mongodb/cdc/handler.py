7266d8b42a28622c7eb2d136ac011ac0e039f60b
Tom Ritchford <tom@swirly.com>
2023-09-18 14:38:18 +0200

    Simplify cdc/handler.py considerably

--- superduperdb/db/mongodb/cdc/handler.py
+++ superduperdb/db/mongodb/cdc/handler.py
@@ -1,17 +1,15 @@
 import queue
 import threading
 import traceback
-import typing as t
 
 from superduperdb import logging
 from superduperdb.container.job import FunctionJob
-from superduperdb.container.serializable import Serializable
 from superduperdb.container.task_workflow import TaskWorkflow
 from superduperdb.db.base.db import DB
 from superduperdb.misc.runnable.queue_chunker import QueueChunker
 from superduperdb.misc.runnable.runnable import Event
 
-from .base import DBEvent, Packet
+from .base import Packet
 from .vector_task_factory import vector_task_factory
 
 queue_chunker = QueueChunker(chunk_size=100, timeout=0.2)
@@ -36,101 +34,54 @@ class CDCHandler(threading.Thread):
 
         threading.Thread.__init__(self, daemon=False)
 
-    def submit_task_workflow(
-        self, cdc_query: t.Optional[Serializable], ids: t.Sequence, task: str = "copy"
-    ) -> None:
-        """submit_task_workflow.
-        A fxn to build a taskflow and execute it with changed ids.
-        This also extends the task workflow graph with a node.
-        This node is responsible for applying a vector indexing listener,
-        and copying the vectors into a vector search database.
-
-        :param cdc_query: A query which will be used by `db._build_task_workflow` method
-        to extract the desired data.
-        :param ids: List of ids which were observed as changed document.
-        :param task: A task name to be executed on vector db.
-        """
-        if task == "delete":
-            task_workflow = TaskWorkflow(self.db)
-        else:
-            task_workflow = self.db._build_task_workflow(
-                cdc_query, ids=ids, verbose=False
-            )
-
-        task_workflow = self.create_vector_listener_task(
-            task_workflow, cdc_query=cdc_query, ids=ids, task=task
-        )
-        task_workflow.run_jobs()
-
-    def create_vector_listener_task(
-        self,
-        task_workflow: TaskWorkflow,
-        cdc_query: t.Optional[Serializable],
-        ids: t.Sequence[str],
-        task: str = 'copy',
-    ) -> TaskWorkflow:
-        """create_vector_listener_task.
-        A helper function to define a node in taskflow graph which is responsible for
-        executing the defined ``task`` on a vector db.
-
-        :param task_workflow: A DiGraph task flow which defines task on a di graph.
-        :param db: A superduperdb instance.
-        :param cdc_query: A basic find query to get cursor on collection.
-        :param ids: A list of ids observed during the change
-        :param task: A task name to be executed on vector db.
-        """
-        from superduperdb.container.vector_index import VectorIndex
-
-        task_callable, task_name = vector_task_factory(task=task)
-        serialized_cdc_query = cdc_query.serialize() if cdc_query else None
-        for identifier in self.db.show('vector_index'):
-            vector_index = self.db.load(identifier=identifier, type_id='vector_index')
-            vector_index = t.cast(VectorIndex, vector_index)
-            assert not isinstance(vector_index.indexing_listener, str)
-            indexing_listener_identifier = vector_index.indexing_listener.identifier
-            task_workflow.add_node(
-                f'{task_name}({indexing_listener_identifier})',
-                job=FunctionJob(
-                    callable=task_callable,
-                    args=[indexing_listener_identifier, serialized_cdc_query, ids],
-                    kwargs={},
-                ),
-            )
-        if task != 'delete':
-            assert indexing_listener_identifier
-            model, _, key = indexing_listener_identifier.rpartition('/')
-            task_workflow.add_edge(
-                f'{model}.predict({key})',
-                f'{task_name}({indexing_listener_identifier})',
-            )
-        return task_workflow
-
-    def on_create(self, packet: Packet) -> None:
-        ids = packet.ids
-        cdc_query = packet.query
-        self.submit_task_workflow(cdc_query=cdc_query, ids=ids)
-
-    def on_update(self, packet: Packet) -> None:
-        self.on_create(packet)
-
-    def on_delete(self, packet: Packet) -> None:
-        ids = packet.ids
-        cdc_query = packet.query
-        self.submit_task_workflow(cdc_query=cdc_query, ids=ids, task="delete")
-
-    def _handle(self, packet: Packet) -> None:
-        if packet.event_type == DBEvent.insert:
-            self.on_create(packet)
-        elif packet.event_type == DBEvent.update:
-            self.on_update(packet)
-        elif packet.event_type == DBEvent.delete:
-            self.on_delete(packet)
-
     def run(self):
         try:
             for c in queue_chunker(CDC_QUEUE, self._stop_event):
-                self._handle(Packet.collate(c))
+                _submit_task_workflow(self.db, Packet.collate(c))
 
         except Exception as exc:
             traceback.print_exc()
             logging.info(f'Error while handling cdc batches :: reason {exc}')
+
+
+def _submit_task_workflow(db: DB, packet: Packet) -> None:
+    """
+    Build a taskflow and execute it with changed ids.
+
+    This also extends the task workflow graph with a node.
+    This node is responsible for applying a vector indexing listener,
+    and copying the vectors into a vector search database.
+    """
+    if packet.is_delete:
+        workflow = TaskWorkflow(db)
+    else:
+        workflow = db._build_task_workflow(packet.query, ids=packet.ids, verbose=False)
+
+    task = 'delete' if packet.is_delete else 'copy'
+    task_callable, task_name = vector_task_factory(task=task)
+
+    serialized_query = packet.query.serialize() if packet.query else None
+
+    def add_node(identifier):
+        from superduperdb.container.vector_index import VectorIndex
+
+        vi = db.load(identifier=identifier, type_id='vector_index')
+        assert isinstance(vi, VectorIndex)
+
+        assert not isinstance(vi.indexing_listener, str)
+        listener_id = vi.indexing_listener.identifier
+
+        args = [listener_id, serialized_query, packet.ids]
+        job = FunctionJob(callable=task_callable, args=args)
+        workflow.add_node(f'{task_name}({listener_id})', job=job)
+
+        return listener_id
+
+    listener_ids = [add_node(i) for i in db.show('vector_index')]
+    if not packet.is_delete:
+        assert listener_ids
+        listener_id = listener_ids[-1]
+        model, _, key = listener_id.rpartition('/')
+        workflow.add_edge(f'{model}.predict({key})', f'{task_name}({listener_id})')
+
+    workflow.run_jobs()

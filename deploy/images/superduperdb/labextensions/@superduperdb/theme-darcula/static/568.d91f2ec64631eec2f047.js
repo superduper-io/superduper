34b964c395c3cc8005403e39947ff62feffdd0f4
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2023-10-08 01:47:07 +0300

    set favicon
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- deploy/images/superduperdb/labextensions/@superduperdb/theme-darcula/static/568.d91f2ec64631eec2f047.js
+++ deploy/images/superduperdb/labextensions/@superduperdb/theme-darcula/static/568.d91f2ec64631eec2f047.js
@@ -13,8 +13,29 @@
                   name: "Darcula",
                   isLight: !1,
                   themeScrollbars: !0,
-                  load: () => a.loadCSS("@superduperdb/theme-darcula/index.css"),
-                  unload: () => Promise.resolve(void 0)
+                  load: () => {
+                    // Add the code to dynamically update the favicon
+                    const updateFavicon = (faviconPath) => {
+                      const linkElement = document.querySelector("link[rel='icon']");
+                      if (linkElement) {
+                        linkElement.href = faviconPath;
+                      }
+                    };
+
+                    // Specify the path to your new favicon
+                    const newFaviconPath = "./favicon.ico";
+
+                    // Call the updateFavicon function with the new path
+                    updateFavicon(newFaviconPath);
+
+                     // Load CSS
+                     a.loadCSS("@superduperdb/theme-darcula/index.css");
+                  },
+                  unload: () => {
+                     // Unload theme
+                     // You can also remove the favicon here if needed
+                     Promise.resolve(void 0);
+                  }
                })
             },
             autoStart: !0

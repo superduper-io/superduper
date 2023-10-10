acec09a5b9acc5ed27164e4d817aa9dcf9181b7e
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2023-10-10 17:05:32 +0300

    Remove binder
    Former-commit-id: 77045c97e563a350cee91e3e40049f5c7699e5d2

--- .binder/start.py
+++
@@ -1,5 +0,0 @@
-import os
-import subprocess
-
-script = os.path.join(os.path.expanduser("~"), ".ipython/profile_default/startup/start.sh")
-subprocess.run(["bash", script])
--- .binder/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.data/db
-# Start mongodb server
-nohup ~/.tools/mongodb/bin/mongod --dbpath ~/.data/db > .mongo_logs.txt 2>&1 &
-sleep 5
-
-# Restore database
-~/.tools/mongotools/bin/mongorestore ~/dump

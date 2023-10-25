c53f371eeb77c39fd1f866e9895b272b9ee48d66
blythed <duncanblythe@gmail.com>
2023-10-25 12:11:16 +0200

    Add components as top-level imports

--- superduperdb/__init__.py
+++ superduperdb/__init__.py
@@ -1,8 +1,6 @@
 from .base import config, configs, jsonable, logger
 from .misc.superduper import superduper
 
-__all__ = 'CFG', 'ICON', 'JSONable', 'ROOT', 'config', 'log', 'logging', 'superduper'
-
 ICON = '🔮'
 CFG = configs.CFG
 JSONable = jsonable.JSONable
@@ -11,3 +9,18 @@ ROOT = configs.ROOT
 logging = log = logger.logging
 
 __version__ = '0.0.13'
+
+from .components.encoder import Encoder
+from .components.model import Model
+from .components.listener import Listener
+from .components.vector_index import VectorIndex
+from .components.dataset import Dataset
+from .components.metric import Metric
+from .components.schema import Schema
+from .components.serializer import Serializer
+
+__all__ = (
+    'CFG', 'ICON', 'JSONable', 'ROOT', 'config', 'log', 'logging', 'superduper',
+    'Encoder', 'Model', 'Listener', 'VectorIndex', 'Dataset', 'Metric', 'Schema',
+    'Serializer',
+)

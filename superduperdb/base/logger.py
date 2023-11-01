62be656bffc9fa51ed8ab563f8cd2624d3ce6f98
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2023-11-01 15:30:24 +0200

    document logger
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- superduperdb/base/logger.py
+++ superduperdb/base/logger.py
@@ -1,3 +1,9 @@
+import os
+from sys import stderr, stdout
+
+from loguru import logger
+from loki_logger_handler.loki_logger_handler import LoguruFormatter, LokiLoggerHandler
+
 from superduperdb.base.config import LogLevel, LogType
 
 from .configs import CFG
@@ -6,12 +12,17 @@ __all__ = ('Logging',)
 
 
 class Logging:
-    if CFG.logging.type == LogType.SYSTEM:
-        from sys import stderr, stdout
+    if CFG.logging.type == LogType.LOKI:  # Send logs to Loki
+        custom_handler = LokiLoggerHandler(
+            url=os.environ["LOKI_URI"],
+            labels={"application": "Test", "environment": "Develop"},
+            labelKeys={},
+            timeout=10,
+            defaultFormatter=LoguruFormatter(),
+        )
 
-        from loguru import logger
-
-        # Replace default with SuperDuperDB settings
+        logger.configure(handlers=[{"sink": custom_handler, "serialize": True}])
+    else:
         logger.remove()
         fmt = "<green>{time:YYYY-MMM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{line}</cyan> | <cyan>{extra}</cyan> <level> | {message} </level> "
 
@@ -22,34 +33,9 @@ class Logging:
         # Send "ERROR" and below to stdout, with "ERROR" and above moved to stderr
         # logger.add(sys.stdout, filter=lambda record: record["level"].no < 40, level="INFO")
 
-        # Set log levels
-        debug = logger.debug
-        info = logger.info
-        success = logger.success
-        warn = logger.warning
-        error = logger.error
-    else:
-        import os
-
-        from loguru import logger
-        from loki_logger_handler.loki_logger_handler import (
-            LoguruFormatter,
-            LokiLoggerHandler,
-        )
-
-        custom_handler = LokiLoggerHandler(
-            url=os.environ["LOKI_URI"],
-            labels={"application": "Test", "environment": "Develop"},
-            labelKeys={},
-            timeout=10,
-            defaultFormatter=LoguruFormatter(),
-        )
-
-        logger.configure(handlers=[{"sink": custom_handler, "serialize": True}])
-
-        # Set log levels
-        debug = logger.debug
-        info = logger.info
-        success = logger.success
-        warn = logger.warning
-        error = logger.error
+    # Set log levels
+    debug = logger.debug
+    info = logger.info
+    success = logger.success
+    warn = logger.warning
+    error = logger.error

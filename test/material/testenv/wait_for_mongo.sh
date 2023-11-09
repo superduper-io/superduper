a015246eeaa0b9a1f5647c15c810fc7b84222923
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2023-11-09 20:14:14 +0200

    fix waiting script
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- test/material/testenv/wait_for_mongo.sh
+++ test/material/testenv/wait_for_mongo.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Check if mongo-init has exited (indicating mongo initialization)
-result=$(docker-compose ps -q --status=exited mongo-init)
+# Maximum number of retries
+max_retries=10
 
-# Count the number of lines in the result
-line_count=$(echo "$result" | wc -l)
+for ((i=1; i<=""$max_retries""; i++)); do
+  # Check if mongo-init has exited (indicating mongo initialization)
+  result=$(docker-compose ps -qa --status=exited mongo-init)
 
-# Check the number of lines
-if [ "$line_count" -eq 0 ]; then
-  echo "Waiting for mongo to be initialized..."
-  sleep 3
-elif [ "$line_count" -eq 1 ]; then
-  echo "Mongo has been initialized successfully."
-  exit 0
-else
-  echo "Unexpected number of mongo-init instances: $line_count"
-  exit 255
-fi
+  # Count the number of lines in the result
+  line_count=$(echo "$result" | wc -l)
+
+  # Check the number of lines
+  if [ "$line_count" -eq 0 ]; then
+    echo "Waiting for mongo to be initialized (Attempt $i)..."
+    sleep 3
+  elif [ "$line_count" -eq 1 ]; then
+    echo "Mongo has been initialized successfully."
+    exit 0
+  else
+    echo "Unexpected number of mongo-init instances: $line_count"
+    exit 255
+  fi
+done
+
+# If the maximum number of retries is reached
+echo "Mongo initialization failed after $max_retries attempts."
+exit 1

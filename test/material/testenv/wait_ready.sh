ac8e5fea8308fd92f212960d442f2639ad26dd66
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2023-11-16 18:39:34 +0200

    add hostnames
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
    
    Former-commit-id: 02b4a424aceab266a73251717e906469f19744cd

--- test/material/testenv/wait_ready.sh
+++ test/material/testenv/wait_ready.sh
@@ -34,7 +34,7 @@ wait_for_exit() {
 
 
 # Define an array
-service_list=("mongo-init" "vector-search-init")
+service_list=("mongo-init" "vector-search-init" "cdc-init")
 
 # Wait for all the services to become ready.
 for service_name in "${service_list[@]}"; do

172ce9388b1ffd88225bf142e8fc1056d4f75167
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2023-11-16 17:13:17 +0200

    fix endpoints
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- test/material/testenv/_local_env_init.sh
+++ test/material/testenv/_local_env_init.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 echo "Starting Mongo Server"
-docker compose up mongodb --detach --remove-orphans
-
-echo "Setting host names"
-./set_hosts.sh
-
-echo "Initializing Mongo Server"
-docker compose up mongo-init
+docker compose up mongodb mongo-init --detach --remove-orphans
 
 echo "Waiting for Mongo to become ready"
 ./wait_ready.sh
+
+echo "Map container names to localhost"
+./set_hosts.sh
 
 # Start everything from the root
 cd ../../../
@@ -21,10 +18,10 @@ echo "Starting Dask Scheduler"
 dask scheduler &
 
 echo "Starting Dask Worker"
-SUPERDUPERDB_DATA_BACKEND='mongodb://superduper:superduper@localost:27017/test_db' dask worker "tcp://localhost:8786" &
+SUPERDUPERDB_DATA_BACKEND='mongodb://superduper:superduper@mongodb:27017/test_db' dask worker "tcp://localhost:8786" &
 
 echo "Starting Vector Search Service"
-SUPERDUPERDB_DATA_BACKEND='mongodb://superduper:superduper@localost:27017/test_db' python -m superduperdb vector-search &
+SUPERDUPERDB_DATA_BACKEND='mongodb://superduper:superduper@mongodb:27017/test_db' python -m superduperdb vector-search &
 
 echo "Starting Change Data Capture (CDC) Service"
-SUPERDUPERDB_DATA_BACKEND='mongodb://superduper:superduper@localost:27017/test_db' python -m superduperdb cdc
+SUPERDUPERDB_DATA_BACKEND='mongodb://superduper:superduper@mongodb:27017/test_db' python -m superduperdb cdc

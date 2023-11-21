ee2d8a4c5c4dfdaaffe989537af23773651a7db1
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2023-11-22 01:59:03 +0200

    add local dask, when distributed is disabled
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- superduperdb/base/build.py
+++ superduperdb/base/build.py
@@ -86,6 +86,8 @@ def build_datalayer(cfg=None, **kwargs) -> Datalayer:
             serializers=cfg.cluster.serializers,
             deserializers=cfg.cluster.deserializers,
         )
+    else:
+        compute = DaskComputeBackend("", local=True)
 
     # Build DataLayer
     # ------------------------------

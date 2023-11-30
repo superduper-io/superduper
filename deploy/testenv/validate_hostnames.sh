49de7034d4d5af918d99f85ff9a8fa66a70edd40
thejumpman2323 <kartik@superduperdb.com>
2023-12-01 00:44:40 +0530

    Add sandbox example with fixes for job creation and submission

--- deploy/testenv/validate_hostnames.sh
+++ deploy/testenv/validate_hostnames.sh
@@ -7,8 +7,11 @@ then
    echo "mongodb found";
 else
   echo "****************************"
-  echo -e "You need to update /etc/hosts:\n"
-  echo "echo 127.0.0.1 mongodb | sudo tee -a /etc/hosts"
+  echo -e "You need to update /etc/hosts with these lines:\n"
+  echo "127.0.0.1 mongodb"
+  echo "127.0.0.1 vector-search"
+  echo "127.0.0.1 cdc"
+  echo "127.0.0.1 scheduler"
   echo "****************************"
   exit 255
 fi

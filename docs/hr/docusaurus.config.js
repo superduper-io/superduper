a81dd88bc611c161de01b080558de8d05f2e6db1
Manmit Tiwade <112580293+Manmit124@users.noreply.github.com>
2023-12-20 23:09:29 +0530

    adding the valid url

--- docs/hr/docusaurus.config.js
+++ docs/hr/docusaurus.config.js
@@ -181,7 +181,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/SuperDuperDB/superduperdb/tree/main/docs',
+            'https://github.com/SuperDuperDB/superduperdb/blob/main/docs',
         },
         blog: {
           showReadingTime: true,

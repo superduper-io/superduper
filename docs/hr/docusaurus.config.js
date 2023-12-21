18ea706d016e0cd06d7fb16e0fd0a6ebcae683f6
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2023-12-21 17:44:45 +0200

    update edit link
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- docs/hr/docusaurus.config.js
+++ docs/hr/docusaurus.config.js
@@ -181,10 +181,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-
             'https://github.com/SuperDuperDB/superduperdb/blob/main/docs/hr',
-
-            'https://github.com/SuperDuperDB/superduperdb/blob/main/docs',
         },
         blog: {
           showReadingTime: true,

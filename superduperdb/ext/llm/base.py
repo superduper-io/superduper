f88604146b74481c5c30dd112b8be15d1accefe7
jieguangzhou <jalonzjg@gmail.com>
2023-12-27 18:18:00 +0800

    Supert vllm model and runing vllm on ray

--- superduperdb/ext/llm/base.py
+++ superduperdb/ext/llm/base.py
@@ -1,6 +1,6 @@
-import abc
 import dataclasses as dc
 import typing as t
+from functools import wraps
 
 from superduperdb.components.component import Component
 from superduperdb.components.model import _Predictor
@@ -8,19 +8,17 @@ from superduperdb.ext.utils import format_prompt
 
 
 @dc.dataclass
-class LLMModel(Component, _Predictor, metaclass=abc.ABCMeta):
-    model_name: t.Optional[str] = dc.field(default="", repr=False)
+class LLMBase(Component, _Predictor):
     max_tokens: int = 512
     temperature: float = 0.0
     prompt_template: str = "{input}"
-    prompt_func: t.Optional[t.Callable] = dc.field(default=None, repr=False)
+    prompt_func: t.Optional[t.Callable] = dc.field(default=None)
 
     def __post_init__(self):
         super().__post_init__()
         self.takes_context = True
         assert "{input}" in self.prompt_template, "Template must contain {input}"
 
-    @abc.abstractmethod
     def _predict_one(self, X, **kwargs):
         pass
 
@@ -41,5 +39,37 @@ class LLMModel(Component, _Predictor, metaclass=abc.ABCMeta):
 
 
 @dc.dataclass
-class LLMAPI(LLMModel):
-    api_url: str = dc.field(default="", repr=False)
+class LLMAPI(LLMBase):
+    api_url: str = dc.field(default="")
+
+    def __post_init__(self):
+        super().__post_init__()
+        assert self.api_url, "api_url can not be empty"
+
+
+def ensure_initialized(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if not hasattr(self, '_is_initialized') or not self._is_initialized:
+            print("Start loading model")
+            self.init()
+            self._is_initialized = True
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+
+@dc.dataclass
+class LLMModel(LLMBase):
+    model_name: str = dc.field(default="")
+    on_ray: bool = False
+    ray_config: dict = dc.field(default_factory=dict)
+
+    def __post_init__(self):
+        super().__post_init__()
+        assert self.model_name, "model_name can not be empty"
+        self._is_initialized = False
+
+    @ensure_initialized
+    def _predict(self, X, one: bool = False, **kwargs):
+        return super()._predict(X, one, **kwargs)

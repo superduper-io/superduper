4d89ebc4499361740e2511d4ecf808d7ed81f9b5
JieguangZhou <jieguang_zhou@163.com>
2023-12-29 23:28:06 +0800

    Organize the import of llm class

---
+++ superduperdb/ext/llm/__init__.py
@@ -0,0 +1,9 @@
+from superduperdb.ext.llm.openai import OpenAI
+from superduperdb.ext.llm.vllm import VllmAPI, VllmModel, VllmOpenAI
+
+__all__ = [
+    "OpenAI",
+    "VllmAPI",
+    "VllmModel",
+    "VllmOpenAI",
+]

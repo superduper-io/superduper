1161b972a897998a76b9d0321ff9e358a6132a0f
thejumpman2323 <kartik@superduperdb.com>
2023-12-31 00:15:58 +0530

    Added Mongodb Atlas vector searcher

--- superduperdb/backends/base/backends.py
+++ superduperdb/backends/base/backends.py
@@ -7,6 +7,7 @@ from superduperdb.backends.mongodb.artifacts import MongoArtifactStore
 from superduperdb.backends.mongodb.data_backend import MongoDataBackend
 from superduperdb.backends.mongodb.metadata import MongoMetaDataStore
 from superduperdb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
+from superduperdb.vector_search.atlas import MongoAtlasVectorSearcher
 from superduperdb.vector_search.in_memory import InMemoryVectorSearcher
 from superduperdb.vector_search.lance import LanceVectorSearcher
 
@@ -28,6 +29,7 @@ metadata_stores = {
 vector_searcher_implementations = {
     'lance': LanceVectorSearcher,
     'in_memory': InMemoryVectorSearcher,
+    'mongodb+srv': MongoAtlasVectorSearcher,
 }
 
 CONNECTIONS = {

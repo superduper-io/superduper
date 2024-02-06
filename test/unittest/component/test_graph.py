d7db34ff3cf72b332559c967d06a6fa53eeb13fb
thejumpman2323 <kartik@superduperdb.com>
2024-02-06 17:44:04 +0530

    Added serialization support in graph

--- test/unittest/component/test_graph.py
+++ test/unittest/component/test_graph.py
@@ -1,5 +1,6 @@
 from test.db_config import DBConfig
 
+import networkx as nx
 import pytest
 
 from superduperdb.components.graph import Graph
@@ -50,30 +51,21 @@ def model3(db):
     yield model
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
-def test_simple_graph(db, model1, model2):
-    g = Graph(identifier='simple-graph', db=db)
+def test_simple_graph(model1, model2):
+    g = Graph(identifier='simple-graph')
     intermediate_node = g.connect(g, model1)
     g.connect(model1, model2)
     assert g.predict(1, one=True) == 4
     assert intermediate_node.output == 2
 
-    # with names
-    g = Graph(identifier='simple-graph-name', db=db)
-    g.connect(g, 'm1')
-    g.connect('m1', 'm2')
-    assert g.predict(1, one=True) == 4
-
-    g = Graph(identifier='simple-graph', db=db)
+    g = Graph(identifier='simple-graph')
     intermediate_node = g.connect(g, model1)
     g.connect(model1, model2)
     assert g.predict([1, 2, 3], one=False) == [4, 5, 6]
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
-def test_complex_graph(db, model1, model2_multi, model3):
-    db = db
-    g = Graph(identifier='complex-graph', db=db)
+def test_complex_graph(model1, model2_multi, model3):
+    g = Graph(identifier='complex-graph')
     g.connect(g, model1)
     g.connect(g, model2_multi, on='x')
     g.connect(model1, model2_multi, on='y')
@@ -84,47 +76,53 @@ def test_complex_graph(db, model1, model2_multi, model3):
     assert g.predict([1, 2, 3], one=False) == [10, 13, 16]
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
-def test_non_dag(db, model1, model2):
-    db = db
+def test_non_dag(model1, model2):
     with pytest.raises(TypeError) as excinfo:
-        g = Graph(identifier='complex-graph', db=db)
+        g = Graph(identifier='complex-graph')
         g.connect(g, model1)
         g.connect(model1, model2)
         g.connect(model2, model1)
         assert 'The graph is not DAG' in str(excinfo.value)
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
-def test_disconnected_edge(db, model1, model2_multi):
-    db = db
+def test_disconnected_edge(model1, model2_multi):
     with pytest.raises(TypeError) as excinfo:
-        g = Graph(identifier='complex-graph', db=db)
+        g = Graph(identifier='complex-graph')
         g.connect(g, model1)
         g.connect(model1, model2_multi, on='x')
         g.predict(1)
         assert 'Graph disconnected at Node: m2_multi' in str(excinfo.value)
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
-def test_no_root(db, model1, model2_multi):
-    db = db
+def test_no_root(model1, model2_multi):
     with pytest.raises(TypeError) as excinfo:
-        g = Graph(identifier='complex-graph', db=db)
+        g = Graph(identifier='complex-graph')
         g.connect(model1, model2_multi, on='x')
         g.predict(1)
         assert 'Root graph node is not present' in str(excinfo.value)
 
 
 def test_complex_graph_with_select(db):
-    g = Graph(identifier='complex-graph', db=db)
-    g.connect(g, 'linear_a')
-    g.connect('linear_a', 'linear_b')
+    g = Graph(identifier='complex-graph')
+    linear_b = db.load('model', 'linear_b')
+    linear_a = db.load('model', 'linear_a')
+    g.connect(g, linear_a)
+    g.connect(linear_a, linear_b)
 
     from superduperdb.backends.mongodb import Collection
 
     select = Collection('documents').find({})
-    g.predict(X='x', db=db, select=select)
+    g.predict(X='x', select=select, db=db)
     assert all(
         ['complex-graph' in x['_outputs']['x'] for x in list(db.execute(select))]
     )
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_serialization(db, model1):
+    g = Graph(identifier='complex-graph')
+    g.connect(g, model1, on='x')
+    original_g = g.G
+    db.add(g)
+    g = db.load('graph', 'complex-graph')
+    assert nx.utils.graphs_equal(original_g, g.G)

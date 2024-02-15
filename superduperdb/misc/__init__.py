c4ee023f0409c2266df44a353ba3cd8230718555
thejumpman2323 <kartik@superduperdb.com>
2024-02-15 14:47:42 +0530

    Add repr in Model dataclass

---
+++ superduperdb/misc/__init__.py
@@ -0,0 +1,14 @@
+# https://stackoverflow.com/questions/39969064/how-to-print-a-message-box-in-python
+def border_msg(msg, indent=1, width=None, title=None):
+    """Print message-box with optional title."""
+    lines = msg.split('\n')
+    space = " " * indent
+    if not width:
+        width = max(map(len, lines))
+    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
+    if title:
+        box += f'║{space}{title:<{width}}{space}║\n'  # title
+        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
+    box += ''.join([f'║{space}{line:<{width}}{space}║\n' for line in lines])
+    box += f'╚{"═" * (width + indent * 2)}╝'  # lower_border
+    return box

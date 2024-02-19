daa6f043f73f07338e3ef64e52105ae519d91efa
thejumpman2323 <kartik@superduperdb.com>
2024-02-19 17:23:23 +0530

    Remove graph node use model instead

--- superduperdb/components/graph.py
+++ superduperdb/components/graph.py
@@ -1,158 +1,30 @@
 import dataclasses as dc
 import typing as t
 from collections import OrderedDict
-from functools import cached_property, partial
-from inspect import signature
 
 import networkx as nx
 
-from superduperdb.base.artifact import Artifact
 from superduperdb.components.component import Component
 from superduperdb.components.model import Model, _Predictor
-from superduperdb.ext.torch.model import TorchModel
 
 
-class KeyStore:
-    def __init__(self, model, params):
-        self.model = model
-        self._params = params
-        for param in params:
-            setattr(self, param, f'{model}.{param}')
-
-    def __len__(self):
-        return len(self._params)
-
-    @property
-    def params(self):
-        return self._params
-
-
-class RootNode:
+class GraphModel:
     def __init__(self, name):
-        self.input = KeyStore('root', [])
         self.identifier = name
-        self.required_input = 0
+        self.inputs = []
 
     def predict(self, x):
         return x
 
 
-class Node(RootNode):
-    def __init__(self, model: t.Union['Model', 'TorchModel']):
-        self.identifier = model.identifier
-        self.encoder = model.encoder
-        self.output_schema = model.output_schema
-        self._output = None
-
-        predict_method = model.predict_method
-        if isinstance(model, TorchModel):
-            predict_method = model.forward_method
-
-        assert isinstance(model.object, Artifact)
-        self.object = model.object.artifact
-        self.model = model
-        self._generic_model = type(model) == Model
-
-        if predict_method:
-            self.predict_method = getattr(self.object, predict_method)
-        else:
-            self.predict_method = self.object
-
-        self.predict_kwargs = model.predict_kwargs or {}
-        self.predict_method = partial(self.predict_method, **self.predict_kwargs)
-
-        # TODO: CAUTION: check if this is not override model.
-        if predict_method:
-            setattr(self.object, predict_method, self.predict_method)
-        else:
-            self.object = self.predict_method
-
-        params = self.set_input()
-        self.input = KeyStore(model.identifier, params)
-
-    def set_input(self):
-        sig = signature(self.predict_method)
-        sig_keys = list(sig.parameters.keys())
-        params = []
-        for k in sig_keys:
-            if k in self.predict_kwargs or (
-                k == 'kwargs' and sig.parameters[k].kind == 4
-            ):
-                continue
-            params.append(k)
-        return params
-
-    def predict(self, *args, one=False, **kwargs):
-        if self._generic_model:
-            self.output = self._forward(*args, one=one, **kwargs)
-        else:
-            self.output = self.model._predict(*args, one=one, **kwargs)
-        return self.output
-
-    def _predict(self, *args, **kwargs):
-        outputs = self.predict_method(*args, **kwargs)
-        return outputs
-
-    def _forward(
-        self, *args, one=False, num_workers: int = 0, **kwargs
-    ) -> t.Sequence[int]:
-        if self.model.batch_predict or one is True:
-            return self._predict(*args, **kwargs)
-
-        if args and kwargs:
-            raise ValueError(
-                'Args and Kwargs at the same time not supported in graph mode'
-            )
-        if args:
-            return self._mutli_forward_args(*args, num_workers=num_workers)
-        return self._mutli_forward_kargs(num_workers=num_workers, **kwargs)
-
-    def _mutli_forward_kargs(self, num_workers=1, **kwargs):
-        outputs = []
-        if num_workers:
-            import multiprocessing
-
-            pool = multiprocessing.Pool(processes=num_workers)
-            for r in pool.starmap(self._predict, zip(*list(kwargs.values()))):
-                outputs.append(r)
-            pool.close()
-            pool.join()
-        else:
-            for r in zip(*list(kwargs.values())):
-                outputs.append(self._predict(*r))
-        return outputs
-
-    def _mutli_forward_args(self, *args, num_workers=1):
-        outputs = []
-        if num_workers:
-            to_call = partial(self._predict, *args)
-            import multiprocessing
-
-            pool = multiprocessing.Pool(processes=num_workers)
-            for r in pool.map(to_call, zip(*args)):
-                outputs.append(r)
-            pool.close()
-            pool.join()
-        else:
-            for r in zip(*args):
-                outputs.append(self._predict(*r))
-        return outputs
-
-    @cached_property
-    def params(self):
-        return self.input.params
-
-    @cached_property
-    def required_input(self):
-        return len(self.params)
+class IntermediateGraphNode:
+    def __init__(self, node, graph):
+        self.node = node
+        self.G = graph
 
     @property
     def output(self):
-        return self._output
-
-    @output.setter
-    def output(self, output):
-        self._output = output
+        return self.G.get_output(self.node)
 
 
 @dc.dataclass(kw_only=True)
@@ -164,9 +36,8 @@ class Graph(Component, _Predictor):
     _DEFAULT_ARG_WEIGHT: t.ClassVar[str] = '_base'
     type_id: t.ClassVar[str] = 'graph'
 
-    def __post_init__(self):
+    def __post_init__(self, artifacts):
         self.G = nx.DiGraph()
-        self._key_store = {}
         self.nodes = {}
         self._node_output_cache = {}
         self.version = 0
@@ -183,6 +54,7 @@ class Graph(Component, _Predictor):
                     on=on,
                     update_edge=False,
                 )
+        super().__post_init__(artifacts=artifacts)
 
     def connect(
         self,
@@ -196,16 +68,13 @@ class Graph(Component, _Predictor):
 
         if u.identifier not in self.nodes:
             if isinstance(u, Graph):
-                node = RootNode(u.identifier)
+                self.nodes[u.identifier] = GraphModel(self.identifier)
             else:
-                node = Node(u)
-
-            self.nodes[u.identifier] = node
+                self.nodes[u.identifier] = u
             self.G.add_node(u.identifier)
 
         if v.identifier not in self.nodes:
-            node = Node(v)
-            self.nodes[v.identifier] = node
+            self.nodes[v.identifier] = v
             self.G.add_node(v.identifier)
 
         G_ = self.G.copy()
@@ -223,10 +92,13 @@ class Graph(Component, _Predictor):
                 self.models.append(u)
             if v not in self.models:
                 self.models.append(v)
-        return self.nodes[v.identifier]
+        return IntermediateGraphNode(self.nodes[v.identifier], self)
 
     def stash_node_output(self, node, output):
         self._node_output_cache[node.identifier] = output
+
+    def get_output(self, node):
+        return self._node_output_cache[node.identifier]
 
     def group_nodes_by_degree(self, nodes):
         grouped_dict = OrderedDict()
@@ -291,13 +163,13 @@ class Graph(Component, _Predictor):
             arg_nodes = list(map(lambda x: self.nodes[x], nodes))
             node = self.nodes[node]
 
-            if node.required_input != len(arg_nodes):
+            if len(node.inputs) != len(arg_nodes):
                 raise TypeError(
                     f'Graph disconnected at Node: {node.identifier} '
                     f'and is partially connected with {nodes}\n'
-                    f'Required connected node is {node.required_input} '
+                    f'Required connected node is {len(node.inputs)} '
                     f'but got only {len(nodes)}, '
-                    f'Node required params: {node.params}'
+                    f'Node required params: {node.inputs.params}'
                 )
 
     def _predict(self, X: t.Any, one: bool = False, **predict_kwargs):
@@ -311,32 +183,33 @@ class Graph(Component, _Predictor):
         self.validate(path)
         output = None
 
-        # Update graph encoder and outschema as per output node.
+        # Update graph datatype and outschema as per output node.
         output_node = self.nodes[path[-1]]
-        self.encoder = output_node.model.encoder
-        self.output_schema = output_node.model.output_schema
+        self.datatype = output_node.datatype
+        self.output_schema = output_node.output_schema
 
         for graph_node in path:
             node = self.nodes[graph_node]
 
             if graph_node == self.identifier:
-                node.output = X
+                self.stash_node_output(node, X)
                 continue
 
             predecessors = list(self.G.predecessors(graph_node))
 
             arg_nodes = list(map(lambda x: self.nodes[x], predecessors))
-            kwargs = {}
+            node_input = {}
 
             for predecessor, arg in zip(predecessors, arg_nodes):
                 data = self.G.get_edge_data(predecessor, graph_node)
                 key = data['weight']
-                kwargs[key] = arg.output
-            args = []
-            if self._DEFAULT_ARG_WEIGHT in kwargs:
-                args = list(kwargs.values())
-                kwargs = {}
-            output = node.predict(*args, one=one, **kwargs)
+
+                node_input[key] = self.get_output(arg)
+
+            if self._DEFAULT_ARG_WEIGHT in node_input:
+                node_input = node_input['_base']
+            output = node.predict(node_input, one=one)
+            self.stash_node_output(node, output)
 
         return output
 

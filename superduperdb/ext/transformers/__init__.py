b4a34d4e3f4e8c6e96a74a6a5f467809d5041964
JieguangZhou <jieguang_zhou@163.com>
2024-03-21 17:13:26 +0800

    Update some reusable scripts
    - Updated reusable_snippets: build_llm
    
    - Updated reusable_snippets: build_text_embedding_model
    
    - Updated reusable_snippets: create_datatype
    
    - Updated reusable_snippets: connect_to_superduperdb

--- superduperdb/ext/transformers/__init__.py
+++ superduperdb/ext/transformers/__init__.py
@@ -3,6 +3,7 @@ from superduperdb.misc.annotations import requires_packages
 requires_packages(
     ['transformers', '4.29.1'],
     ['datasets', '2.18.0'],
+    ['torch']
 )
 
 from .llm import LLM

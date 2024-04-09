dffa2f2a4da82b932b5baf75892ee23874dba1b3
thejumpman2323 <kartik@superduperdb.com>
2024-04-10 00:24:51 +0530

    Create local cluster with tmux sessions

--- superduperdb/server/cluster.py
+++ superduperdb/server/cluster.py
@@ -2,40 +2,22 @@ import subprocess
 import time
 
 
-def subprocess_run(cmds):
-    print(' '.join(cmds))
-    subprocess.run(cmds)
-
-
 def create_tmux_session(session_name, commands):
     '''
     Create a tmux local cluster
     '''
 
-    window_name = f'{session_name}:0'
-    for ix, cmd in enumerate(commands, start=1):
-        if ix == 1:
-            subprocess_run(
-                [
-                    'tmux',
-                    'new-session',
-                    '-d',
-                    '-s',
-                    session_name,
-                    '-n',
-                    'shell0',
-                    '-d',
-                    cmd,
-                ]
-            )
-        else:
-            subprocess_run(['tmux', 'split-window', '-t', window_name, cmd])
+    for ix, cmd in enumerate(commands, start=0):
+        window_name = f'{session_name}:0.{ix}'
+        run_tmux_command(['send-keys', '-t', window_name, cmd, 'C-m'])
+        time.sleep(1)
 
-        if ix % 4 == 0:
-            subprocess_run(['tmux', 'select-layout', '-t', window_name, 'tiled'])
+    run_tmux_command(['attach-session', '-t', session_name])
 
-        time.sleep(2)
-    subprocess_run(['tmux', 'attach-session', '-t', session_name])
+
+def run_tmux_command(command):
+    print('tmux ' + ' '.join(command))
+    subprocess.run(["tmux"] + command, check=True)
 
 
 def local_cluster():
@@ -43,6 +25,20 @@ def local_cluster():
     session_name = 'superduperdb-localcluster-session'
 
     CFG = 'deploy/testenv/env/smoke/debug.yaml'
+    run_tmux_command(
+        [
+            'new-session',
+            '-d',
+            '-s',
+            session_name,
+        ]
+    )
+
+    run_tmux_command(["split-window", "-h"])
+    run_tmux_command(["split-window", "-v"])
+    run_tmux_command(["select-pane", "-t", "0"])
+    run_tmux_command(["split-window", "-v"])
+
     services = [
         f"SUPERDUPERDB_CONFIG={CFG} PYTHONPATH=$(pwd):. "
         "ray start --head --dashboard-host=0.0.0.0 --disable-usage-stats --block",

7bf835bcef8ca896272fd808f37090a4c46a1b54
Duncan Blythe <duncan@superduperdb.com>
2024-05-02 00:12:59 +0200

    Complete deep_flat_encode

--- superduperdb/base/leaf.py
+++ superduperdb/base/leaf.py
@@ -1,16 +1,47 @@
+import dataclasses as dc
+import importlib
 import inspect
 import typing as t
 from abc import ABC, abstractmethod, abstractproperty
+from superduperdb.misc.serialization import asdict
 
 _CLASS_REGISTRY = {}
 
 
+def _import_item(cls, module, dict):
+    module = importlib.import_module(module)
+    cls = getattr(module, cls)
+    try:
+        return cls(**dict)
+    except TypeError as e:
+        if 'got an unexpected keyword argument' in str(e):
+            if callable(cls):
+                return cls(**{k: v for k, v in dict.items() if k in inspect.signature(cls).parameters})
+            return cls(**{k: v for k, v in dict.items() if k in inspect.signature(cls.__init__).parameters})
+        raise e
+
+
+@dc.dataclass
 class Leaf(ABC):
     """Base class for all leaf classes."""
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         cls._register_class()
+
+    def _deep_flat_encode(self, cache, blobs, files):
+        from superduperdb.base.document import _deep_flat_encode
+        r = dict(self.dict())
+        return _deep_flat_encode(r, cache, blobs, files)
+
+    def dict(self):
+        from superduperdb import Document
+        r = asdict(self)
+        return Document({
+            'module': self.__class__.__module__,
+            'cls': self.__class__.__name__,
+            'dict': r
+        })
 
     @classmethod
     def handle_integration(cls, r):

84c16694bb103ab930d1aa94c8dac70fd8eca39c
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2024-05-16 15:02:54 +0300

    fix credentials and dependencies
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- deploy/databases/oracle/example.py
+++ deploy/databases/oracle/example.py
@@ -1,8 +1,23 @@
-from superduperdb import superduper
+import ibis
 
 user = 'superduper'
 password = 'superduper'
 port = 1521
 host = 'localhost'
+database = 'test_db'
 
-db = superduper(f"oracle://{user}:{password}@{host}:{port}")
+# OK
+con = ibis.oracle.connect(
+    user = 'superduper',
+    password = 'superduper',
+    port = 1521,
+    host = 'localhost',
+    database = 'test_db',
+)
+
+# ERROR AttributeError: 'str' object has no attribute 'username'
+#con = ibis.connect(f"oracle://{user}:{password}@{host}:{port}/{database}")
+
+# ERROR ModuleNotFoundError: No module named 'ibis.backends.base'
+# from superduperdb import superduper
+#db = superduper(f"oracle://{user}:{password}@{host}:{port}/{database}")

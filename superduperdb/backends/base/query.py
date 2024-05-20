0de369f54d28092b5af11d6010d6fd40d1d3d3c4
thejumpman2323 <kartik@superduperdb.com>
2024-05-20 15:18:01 +0530

    Fix new execute notation

--- superduperdb/backends/base/query.py
+++ superduperdb/backends/base/query.py
@@ -368,6 +368,10 @@ class Query(_BaseQuery):
         pass
 
     def execute(self, db=None):
+        """Execute the query with query.execute() notation.
+
+        :param db: Datalayer instance.
+        """
         self.db = db or self.db
         return self.db.execute(self)
 
@@ -574,7 +578,7 @@ class PredictOne(_BaseQuery):
     args: t.Sequence = dc.field(default_factory=list)
     kwargs: t.Dict = dc.field(default_factory=dict)
 
-    def execute(self, db):
+    def do_execute(self, db):
         """Execute the query.
 
         :param db: The datalayer instance

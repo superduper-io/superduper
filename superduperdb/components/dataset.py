09eb468f1f42a29d74d504e1eba9edb36f6f5313
JieguangZhou <jieguang_zhou@163.com>
2024-06-04 16:33:50 +0800

    Optimized the logic of loading dataset data.

--- superduperdb/components/dataset.py
+++ superduperdb/components/dataset.py
@@ -5,17 +5,12 @@ import typing as t
 from functools import cached_property
 
 import numpy
-from overrides import override
 
 from superduperdb.backends.base.query import Query
-from superduperdb.base.datalayer import Datalayer
-from superduperdb.base.document import Document
 from superduperdb.components.component import Component, ensure_initialized
 from superduperdb.components.datatype import (
     DataType,
     dill_serializer,
-    pickle_decode,
-    pickle_encode,
 )
 from superduperdb.misc.annotations import merge_docstrings
 
@@ -29,7 +24,6 @@ class Dataset(Component):
     :param sample_size: The number of documents to sample from the query.
     :param random_seed: The random seed to use for sampling.
     :param creation_date: The date the dataset was created.
-    :param raw_data: The raw data for the dataset.
     """
 
     type_id: t.ClassVar[str] = 'dataset'
@@ -41,7 +35,6 @@ class Dataset(Component):
     sample_size: t.Optional[int] = None
     random_seed: t.Optional[int] = None
     creation_date: t.Optional[str] = None
-    raw_data: t.Optional[t.Sequence[t.Any]] = None
 
     def __post_init__(self, db, artifacts):
         """Post-initialization method.
@@ -60,27 +53,22 @@ class Dataset(Component):
     def init(self, db=None):
         """Initialization method."""
         super().init(db=db)
-        self._data = [
-            Document.decode(r, db=self.db).unpack()
-            for r in pickle_decode(self.raw_data)
-        ]
-
-    @override
-    def pre_create(self, db: 'Datalayer') -> None:
-        """Pre-create hook for database operations.
-
-        :param db: The database to use for the operation.
-        """
-        if self.raw_data is None:
+        if self._data is None:
             if self.select is None:
                 raise ValueError('Select cannot be None')
-            data = list(db.execute(self.select))
+            data = list(self.db.execute(self.select))
             if self.sample_size is not None and self.sample_size < len(data):
                 perm = self.random.permutation(len(data)).tolist()
                 data = [data[perm[i]] for i in range(self.sample_size)]
-            self.raw_data = pickle_encode([r.encode() for r in data])
+            self._data = data
 
     @cached_property
     def random(self):
         """Cached property representing the random number generator."""
         return numpy.random.default_rng(seed=self.random_seed)
+
+    def __str__(self):
+        """String representation of the dataset."""
+        return f'Dataset(identifier={self.identifier}, select={self.select})'
+
+    __repr__ = __str__

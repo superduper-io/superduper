1f6a0b2185e0c4e56b77a7f97c1c3bb2edc12b9f
JieguangZhou <jieguang_zhou@163.com>
2024-07-10 15:57:04 +0800

    Using pip to guide users in installing the new package 'superduper'

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+from setuptools import setup
+
+if not os.getenv("IGNORE_ERROR"):
+    error_message = """
+    ERROR: This package has been renamed to 'superduper'. Please install the new package using:
+    
+    pip install superduper
+
+    Github: https://github.com/SuperDuperDB/superduper
+    """
+    print(error_message)
+    sys.exit(1)
+
+setup(
+    name="superduperdb",
+    version="0.2.2",
+    description="This package has been deprecated. Use superduper instead.",
+    packages=[],
+)

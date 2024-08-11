c4ef130b8de9c88b89cfa0cdbe0227ecf976855d
fazlulkarimweb <fazlulkarimrocky@gmail.com>
2024-08-11 15:50:06 +0000

    Updated edit URL

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -194,7 +194,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/superduper-io/superduper/blob/main/docs/hr',
+            'https://github.com/superduper-io/superduper/blob/main/docs/',
         },
         blog: false,
         theme: {

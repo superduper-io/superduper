fa4981e0ed9e4cd766910d4ad19f69e72627a7f6
Duncan Blythe <duncan@superduperdb.com>
2024-09-09 22:01:38 +0200

    Simplify Job class

--- superduper/jobs/job.py
+++ superduper/jobs/job.py
@@ -1,257 +1,53 @@
+import dataclasses as dc
 import datetime
 import typing as t
 import uuid
-from abc import abstractmethod
 
-import superduper as s
-from superduper import CFG
-from superduper.jobs.tasks import callable_job, method_job
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
-    from superduper.components.component import Component
 
 
-def job(f):
-    """
-    Decorator to create a job from a function.
-
-    :param f: function to be decorated
-    """
-
-    def wrapper(
-        *args,
-        db: t.Any = None,
-        dependencies: t.Sequence[Job] = (),
-        **kwargs,
-    ):
-        j = FunctionJob(callable=f, args=args, kwargs=kwargs)
-        return j(db=db, dependencies=dependencies)
-
-    return wrapper
-
-
+@dc.dataclass
 class Job:
     """
     Base class for jobs. Jobs are used to run functions or methods on.
 
-    :param args: positional arguments to be passed to the function or method
-    :param kwargs: keyword arguments to be passed to the function or method
-    :param identifier: Job identifier.
-    :param db: A datalayer instance.
+    :param type_id: Component type_id
+    :param identifier: Component identifier
+    :param uuid: Component uuid
+    :param method: Method name of Component
+    :param job_id: Job identifier
+    :param args: positional arguments to be passed to the method
+    :param kwargs: keyword arguments to be passed to the method
+    :param db: A datalayer instance
+    :param time: Time of job creation
     """
+    type_id: str
+    identifier: str
+    uuid: str
+    method: str
+    job_id: t.Optional[str] = dc.field(default_factory=lambda: str(uuid.uuid4()))
+    args: t.Sequence = dc.field(default_factory=tuple)
+    kwargs: t.Dict = dc.field(default_factory=dict)
+    db: dc.InitVar['Datalayer'] = None
+    time: datetime.datetime = dc.field(default_factory=lambda: str(datetime.datetime.now()))
 
-    callable: t.Optional[t.Callable]
-
-    def __init__(
-        self,
-        args: t.Optional[t.Sequence] = None,
-        kwargs: t.Optional[t.Dict] = None,
-        identifier: t.Optional[str] = None,
-        db: t.Optional['Datalayer'] = None,
-    ):
-        self.args = args or ()
-        self.kwargs = kwargs or {}
-        self.identifier = identifier or str(uuid.uuid4())
-        self.time = datetime.datetime.now()
-        self.callable = None
-        self.db = None
-        self.future = None
-        self.job_id = self.identifier
-        self.db = db
-
-    # TODO no longer needed
-    def watch(self):
-        """Watch the stdout of the job."""
-        return self.db.metadata.watch_job(identifier=self.identifier)
-
-    @abstractmethod
-    def submit(self, compute, dependencies=()):
-        """Submit job for execution.
-
-        :param compute: compute engine
-        :param dependencies: list of dependencies
-        """
-
-    def dict(self):
-        """Return a dictionary representation of the job."""
-        return {
-            'identifier': self.identifier,
-            'time': self.time,
-            'status': 'pending',
-            'args': self.args,
-            'kwargs': self.kwargs,
-            'stdout': [],
-            'stderr': [],
-            'job_id': self.job_id,
-        }
-
-    @abstractmethod
-    def __call__(self, dependencies=()):
-        """
-        Run the job.
-
-        :param dependencies: list of dependencies
-        """
-
-
-class FunctionJob(Job):
-    """Job for running a function.
-
-    :param callable: function to be called
-    :param args: positional arguments to be passed to the function
-    :param kwargs: keyword arguments to be passed to the function
-    :param identifier: Job identifier.
-    :param db: A datalayer instance.
-    """
-
-    def __init__(
-        self,
-        callable: t.Callable,
-        args: t.Optional[t.Sequence] = None,
-        kwargs: t.Optional[t.Dict] = None,
-        identifier: t.Optional[str] = None,
-        db: t.Optional['Datalayer'] = None,
-    ):
-        super().__init__(args=args, kwargs=kwargs, db=db, identifier=identifier)
-        self.callable = callable
-
-    def dict(self):
-        """Return a dictionary representation of the job."""
-        d = super().dict()
-        d['_path'] = f'superduper.jobs.job.FunctionJob'
-        return d
+    def __post_init__(self, db):
+        self.db: 'Datalayer' = db
 
     def submit(self, dependencies=()):
         """Submit job for execution.
 
         :param dependencies: list of dependencies
         """
-        self.future = self.db.compute.submit(
-            callable_job,
-            cfg=s.CFG.dict(),
-            function_to_call=self.callable,
-            job_id=self.identifier,
-            args=self.args,
-            kwargs=self.kwargs,
-            dependencies=dependencies,
-            db=self.db if self.db.compute.type == 'local' else None,
-        )
-        return
-
-    def __call__(self, db: t.Union['Datalayer', None], dependencies=()):
-        """Run the job.
-
-        :param db: Datalayer instance to use
-        :param dependencies: list of dependencies
-        """
-        if db is None:
-            from superduper.base.build import build_datalayer
-
-            db = build_datalayer()
-
-        self.db = db
-
-        db.metadata.create_job(self.dict())
-        self.submit(dependencies=dependencies)
-        if self.future:
-            db.metadata.update_job(self.job_id, 'job_id', self.future)
-
-        return self
-
-
-class ComponentJob(Job):
-    """
-    Job for running a class method of a component.
-
-    :param component_identifier: identifier of the component
-    :param component_uuid: unique random identifier of the component
-    :param type_id: type of the component
-    :param method_name: name of the method to be called
-    :param args: positional arguments to be passed to the method
-    :param kwargs: keyword arguments to be passed to the method
-    :param compute_kwargs: Arguments to use for model predict computation
-    :param identifier: Job identifier.
-    :param db: A Datalayer instance.
-    :param component: Cached component.
-    """
-
-    # TODO add version information to a Job
-    def __init__(
-        self,
-        component_identifier: str,
-        component_uuid: str,
-        type_id: str,
-        method_name: str,
-        args: t.Optional[t.Sequence] = None,
-        kwargs: t.Optional[t.Dict] = None,
-        compute_kwargs: t.Dict = {},
-        identifier: t.Optional[str] = None,
-        db: t.Optional['Datalayer'] = None,
-        component: 'Component' = None,
-    ):
-        self.compute_kwargs = compute_kwargs or CFG.cluster.compute.kwargs
-
-        super().__init__(args=args, kwargs=kwargs, db=db, identifier=identifier)
-
-        self.component_identifier = component_identifier
-        self.component_uuid = component_uuid
-        self.method_name = method_name
-        self.type_id = type_id
-        self._component = component
-
-    @property
-    def component(self):
-        """Get the component."""
-        return self._component
-
-    @component.setter
-    def component(self, value):
-        """Set the component.
-
-        :param value: component to set
-        """
-        self._component = value
-        self.callable = getattr(self._component, self.method_name)
-
-    def submit(self, dependencies: t.Sequence[str] = ()):
-        """Submit job for execution.
-
-        :param dependencies: list of dependencies
-        """
-        self.future = self.db.compute.submit(
-            method_job,
-            cfg=s.CFG.dict(),
-            type_id=self.type_id,
-            identifier=self.component_identifier,
-            method_name=self.method_name,
-            job_id=self.identifier,
-            args=self.args,
-            kwargs=self.kwargs,
-            db=self.db if self.db.compute.type == 'local' else None,
-            dependencies=dependencies
-        )
-        return self
-
-    def __call__(self, dependencies=()):
-        """Run the job.
-        """
-        self.db.metadata.create_job(self.dict())
-        self.submit(dependencies=dependencies)
-        if self.future:
-            self.db.metadata.update_job(self.job_id, 'job_id', self.future)
-        return
+        return self.db.compute.submit(self, dependencies=dependencies)
 
     def dict(self):
         """Return a dictionary representation of the job."""
-        d = super().dict()
-        d.update(
-            {
-                'method_name': self.method_name,
-                'component_identifier': self.component_identifier,
-                'type_id': self.type_id,
-                'component_uuid': self.component_uuid,
-                '_path': 'superduper.jobs.job.ComponentJob',
-            }
-        )
-        return d
+        return dc.asdict(self)
+
+    def __call__(self, dependencies=()):
+        """Run the job."""
+        self.db.metadata.create_job(self.dict())
+        return self.submit(dependencies=dependencies)

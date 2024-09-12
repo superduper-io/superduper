955dc0f6cfc01750598d904cf7373e8d6ce18aaa
JieguangZhou <jieguang_zhou@163.com>
2024-09-12 21:48:08 +0800

    Update vllm plugin (#2448)

--- plugins/vllm/superduper_vllm/__init__.py
+++ plugins/vllm/superduper_vllm/__init__.py
@@ -1,11 +1,5 @@
-from superduper.misc.annotations import requires_packages
+from superduper_vllm.model import VllmChat, VllmCompletion
 
-from superduper_vllm.model import VllmAPI, VllmModel
+__all__ = ["VllmChat", "VllmCompletion"]
 
-__version__ = "0.0.2"
-
-__all__ = ["VllmAPI", "VllmModel"]
-
-_, requirements = requires_packages(
-    ['vllm', None, None], ['ray', None, None], warn=True
-)
+__version__ = "0.0.1"

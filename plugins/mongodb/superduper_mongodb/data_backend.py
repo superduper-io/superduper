7a1c1f95d998824a524ef2d7ef184d689425d516
JieguangZhou <jieguang_zhou@163.com>
2024-09-20 16:44:47 +0800

    auto schema supports saving raw json data to MongoDB outputs.

--- plugins/mongodb/superduper_mongodb/data_backend.py
+++ plugins/mongodb/superduper_mongodb/data_backend.py
@@ -10,6 +10,7 @@ from superduper.backends.base.data_backend import BaseDataBackend
 from superduper.backends.base.metadata import MetaDataStoreProxy
 from superduper.base.enums import DBType
 from superduper.components.datatype import DataType
+from superduper.components.schema import FieldType, Schema
 from superduper.misc.colors import Colors
 
 from superduper_mongodb.artifacts import MongoArtifactStore
@@ -237,9 +238,14 @@ class MongoDataBackend(BaseDataBackend):
 
         return infer_schema(data, identifier)
 
-    def create_table_and_schema(self, identifier: str, mapping: dict):
+    def create_table_and_schema(self, identifier: str, schema: Schema):
         """Create a table and schema in the data backend.
 
         :param identifier: The identifier for the table
         :param mapping: The mapping for the schema
         """
+        # If the data can be converted to JSON,
+        # then save it as native data in MongoDB.
+        for key, datatype in schema.fields.items():
+            if isinstance(datatype, DataType) and datatype.identifier == "json":
+                schema.fields[key] = FieldType(identifier="json")

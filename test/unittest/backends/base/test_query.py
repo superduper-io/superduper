dc0cec390631bc3ee528e0701cca5a789b63c7ce
JieguangZhou <jieguang_zhou@163.com>
2024-09-20 12:12:11 +0800

    Fix the query parser incompatibility with '.' symbol

--- test/unittest/backends/base/test_query.py
+++ test/unittest/backends/base/test_query.py
@@ -1,5 +1,7 @@
 from test.utils.database import query as query_utils
 
+from superduper import Document
+
 
 def test_insert(db):
     query_utils.test_insert(db)
@@ -31,3 +33,20 @@ def test_model(db):
 
 def test_model_query():
     query_utils.test_model_query()
+
+
+def test_model_query_serialization():
+    query = {
+        "query": 'modela.predict("<var:abc>", documents[0], '
+        'condition={"uri": "123.PDF"})',
+        '_variables': {'abc': "ABC"},
+        "documents": [{"a": "a"}],
+        "_builds": {},
+        "_files": {},
+        "_path": "superduper.backends.base.query.parse_query",
+    }
+
+    decode_query = Document.decode(query)
+    assert decode_query.parts[0][0] == 'predict'
+    assert decode_query.parts[0][1] == ("ABC", {'a': 'a'})
+    assert decode_query.parts[0][2] == {'condition': {'uri': '123.PDF'}}

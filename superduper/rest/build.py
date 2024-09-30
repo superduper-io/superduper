0a1bd2930503fbd58697d78b1cce1330539795c7
TheDude <kartik4949@gmail.com>
2024-10-01 00:33:08 +0530

    Add basic rest fastapi server

---
+++ superduper/rest/build.py
@@ -0,0 +1,134 @@
+import hashlib
+import typing as t
+
+import magic
+from fastapi import File, Response
+
+from superduper import logging
+from superduper.backends.base.query import Query
+from superduper.base.document import Document
+from superduper.components.component import Component
+from superduper.rest.base import SuperDuperApp
+
+from .utils import rewrite_artifacts
+
+
+def build_rest_app(app: SuperDuperApp):
+    """
+    Add the key endpoints to the FastAPI app.
+
+    :param app: SuperDuperApp
+    """
+
+    @app.add('/db/artifact_store/put', method='put')
+    def db_artifact_store_put_bytes(raw: bytes = File(...)):
+        file_id = str(hashlib.sha1(raw).hexdigest())
+        app.db.artifact_store.put_bytes(serialized=raw, file_id=file_id)
+        return {'file_id': file_id}
+
+    @app.add('/db/artifact_store/get', method='get')
+    def db_artifact_store_get_bytes(file_id: str):
+        bytes = app.db.artifact_store.get_bytes(file_id=file_id)
+        media_type = magic.from_buffer(bytes, mime=True)
+        return Response(content=bytes, media_type=media_type)
+
+    @app.add('/db/apply', method='post')
+    def db_apply(info: t.Dict):
+        if '_variables' in info:
+            assert {'_variables', 'identifier'}.issubset(info.keys())
+            variables = info.pop('_variables')
+            for k in variables:
+                assert '<' not in variables[k]
+                assert '>' not in variables[k]
+                assert ' ' not in variables[k]
+
+            identifier = info.pop('identifier')
+            template_name = info.pop('_template_name', None)
+
+            component = Component.from_template(
+                identifier=identifier,
+                template_body=info,
+                template_name=template_name,
+                db=app.db,
+                **variables,
+            )
+            app.db.apply(component)
+            return {'status': 'ok'}
+        component = Document.decode(info).unpack()
+        app.db.apply(component)
+        return {'status': 'ok'}
+
+    @app.add('/db/show', method='get')
+    def db_show(
+        type_id: t.Optional[str] = None,
+        identifier: t.Optional[str] = None,
+        version: t.Optional[int] = None,
+        application: t.Optional[str] = None,
+    ):
+        if application is not None:
+            r = app.db.metadata.get_component('application', application)
+            return r['namespace']
+        else:
+            return app.db.show(
+                type_id=type_id,
+                identifier=identifier,
+                version=version,
+            )
+
+    @app.add('/db/remove', method='post')
+    def db_remove(type_id: str, identifier: str):
+        app.db.remove(type_id=type_id, identifier=identifier, force=True)
+        return {'status': 'ok'}
+
+    @app.add('/db/show_template', method='get')
+    def db_show_template(identifier: str, type_id: str = 'template'):
+        template = app.db.load(type_id=type_id, identifier=identifier)
+        return template.form_template
+
+    @app.add('/db/metadata/show_jobs', method='get')
+    def db_metadata_show_jobs(type_id: str, identifier: t.Optional[str] = None):
+        return [
+            r['job_id']
+            for r in app.db.metadata.show_jobs(
+                type_id=type_id, component_identifier=identifier
+            )
+            if 'job_id' in r
+        ]
+
+    @app.add('/db/execute', method='post')
+    def db_execute(
+        query: t.Dict,
+    ):
+        if '_path' not in query:
+            plugin = app.db.databackend.type.__module__.split('.')[0]
+            query['_path'] = f'{plugin}.query.parse_query'
+
+        q = Document.decode(query, db=app.db).unpack()
+
+        logging.info('processing this query:')
+        logging.info(q)
+
+        result = q.execute()
+
+        if q.type in {'insert', 'delete', 'update'}:
+            return {'_base': [str(x) for x in result[0]]}, []
+
+        logging.warn(str(q))
+
+        if isinstance(result, Document):
+            result = [result]
+
+        result = [rewrite_artifacts(r, db=app.db) for r in result]
+        result = [r.encode() for r in result]
+        blobs_keys = [list(r.pop_blobs().keys()) for r in result]
+        result = list(zip(result, blobs_keys))
+
+        if isinstance(q, Query):
+            for i, r in enumerate(result):
+                r = list(r)
+                if q.primary_id in r[0]:
+                    r[0][q.primary_id] = str(r[0][q.primary_id])
+                result[i] = tuple(r)
+            if 'score' in result[0][0]:
+                result = sorted(result, key=lambda x: -x[0]['score'])
+        return result
---
+++ test/rest/test_rest.py
@@ -0,0 +1,132 @@
+import json
+
+import pytest
+from fastapi.testclient import TestClient
+
+from superduper import CFG
+from superduper.base.document import Document
+
+CFG.auto_schema = True
+CFG.cluster.rest.uri = 'localhost:8000'
+from superduper.rest.deployed_app import app
+
+from .mock_client import setup as _setup, teardown
+
+
+@pytest.fixture
+def setup():
+    client = TestClient(app._app)
+    yield _setup(client)
+    teardown(client)
+
+
+def test_health(setup):
+    response = setup.get("/health")
+    assert response.status_code == 200
+
+
+def test_select_data(setup):
+    result = setup.post('/db/execute', json={'query': 'coll.find({}, {"_id": 0})'})
+    result = json.loads(result.content)
+    if 'error' in result:
+        raise Exception(result['messages'] + result['traceback'])
+    print(result)
+    assert len(result) == 2
+
+
+CODE = """
+from superduper import code
+
+@code
+def my_function(x):
+    return x + 1
+"""
+
+
+def test_apply(setup):
+    m = {
+        '_builds': {
+            'function_body': {
+                '_path': 'superduper.base.code.Code',
+                'code': CODE,
+            },
+            'my_function': {
+                '_path': 'superduper.components.model.ObjectModel',
+                'object': '?function_body',
+                'identifier': 'my_function',
+            },
+        },
+        '_base': '?my_function',
+    }
+
+    _ = setup.post(
+        '/db/apply',
+        json=m,
+    )
+
+    models = setup.get('/db/show', params={'type_id': 'model'})
+    models = json.loads(models.content)
+
+    assert models == ['my_function']
+
+
+@pytest.mark.skip
+def test_insert_image(setup):
+    result = setup.put(
+        '/db/artifact_store/put', files={"raw": ("test/material/data/test.png")}
+    )
+    result = json.loads(result.content)
+
+    file_id = result['file_id']
+
+    query = {
+        '_path': 'superduper.backends.mongodb.query.parse_query',
+        'query': 'coll.insert_one(documents[0])',
+        '_builds': {
+            'image_type': {
+                '_path': 'superduper.ext.pillow.encoder.image_type',
+                'encodable': 'artifact',
+            },
+            'my_artifact': {
+                '_path': 'superduper.components.datatype.LazyArtifact',
+                'blob': f'&:blob:{file_id}',
+                'datatype': "?image_type",
+            },
+        },
+        'documents': [
+            {
+                'img': '?my_artifact',
+            }
+        ],
+    }
+
+    result = setup.post(
+        '/db/execute',
+        json=query,
+    )
+
+    query = {
+        '_path': 'superduper.backends.mongodb.query.parse_query',
+        'query': 'coll.find(documents[0], documents[1])',
+        'documents': [{}, {'_id': 0}],
+    }
+
+    result = setup.post(
+        '/db/execute',
+        json=query,
+    )
+
+    result = json.loads(result.content)
+    from superduper import superduper
+
+    db = superduper()
+
+    result = [Document.decode(r[0], db=db).unpack() for r in result]
+
+    assert len(result) == 3
+
+    image_record = next(r for r in result if 'img' in r)
+
+    from PIL.PngImagePlugin import PngImageFile
+
+    assert isinstance(image_record['img'], PngImageFile)

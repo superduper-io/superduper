fc202a0b2a447a14fcb6be3ab369e6a59fa16b70
Duncan Blythe <duncan@superduperdb.com>
2024-10-14 18:24:22 +0200

    [PLUGINS] Bump Version [openai]

--- plugins/openai/superduper_openai/__init__.py
+++ plugins/openai/superduper_openai/__init__.py
@@ -4,6 +4,6 @@ _, requirements = requires_packages(['openai', '1.1.2', None], ['httpx'])
 
 from .model import OpenAIChatCompletion, OpenAIEmbedding
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 __all__ = 'OpenAIChatCompletion', 'OpenAIEmbedding'

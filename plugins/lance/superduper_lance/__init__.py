ff85d68bbf2cb24c731f5dcc747bf449e9e7d27b
TheDude <kartik4949@gmail.com>
2024-10-22 15:41:20 +0530

    update changelog

--- plugins/lance/superduper_lance/__init__.py
+++ plugins/lance/superduper_lance/__init__.py
@@ -1,3 +1,9 @@
+from superduper.misc.annotations import requires_packages
+
 from .lance import LanceVectorSearcher as VectorSearcher
+
+_, requirements = requires_packages(['pylance', '0.6.1', '0.8.14'])
+
+__version__ = "0.0.6"
 
 __all__ = ['VectorSearcher']

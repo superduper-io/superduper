011ae220c4b588f57b795d4513e314f4840c481b
Duncan Blythe <duncan@superduperdb.com>
2024-10-30 11:12:54 +0100

    Remove requirement of HOME directory

--- superduper/base/config_settings.py
+++ superduper/base/config_settings.py
@@ -11,12 +11,17 @@ from .config import _dataclass_from_dict
 
 File = t.Union[Path, str]
 
+HOME = os.environ.get('HOME')
+
 PREFIX = 'SUPERDUPER_'
 CONFIG_FILE = os.environ.get('SUPERDUPER_CONFIG')
 USER_CONFIG: str = (
     str(Path(CONFIG_FILE).expanduser())
     if CONFIG_FILE
-    else f'{os.environ["HOME"]}/.superduper/config.yaml'
+    else (
+        f'{HOME}/.superduper/config.yaml' if HOME
+        else None
+    )
 )
 PREFIX = 'SUPERDUPER_'
 ROOT = Path(__file__).parents[2]

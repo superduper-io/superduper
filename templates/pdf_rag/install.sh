a2673df9ca6a75e0479ba39806b17df876c4485f
Duncan Blythe <duncan@superduperdb.com>
2025-01-13 15:48:31 +0100

    Bump Version 0.5.0

--- templates/pdf_rag/install.sh
+++ templates/pdf_rag/install.sh
@@ -1,5 +1,5 @@
-apt update
-apt install -y libgl1-mesa-glx
-apt install -y poppler-utils
-apt install -y tesseract-ocr
-python3 -c 'import nltk; nltk.download("punkt"); nltk.download("averaged_perceptron_tagger")'
+sudo apt update
+sudo apt install -y libgl1-mesa-glx
+sudo apt install -y poppler-utils
+sudo apt install -y tesseract-ocr
+python -c 'import nltk; nltk.download("punkt"); nltk.download("averaged_perceptron_tagger"); nltk.download("punkt_tab"); nltk.download("averaged_perceptron_tagger_eng");'

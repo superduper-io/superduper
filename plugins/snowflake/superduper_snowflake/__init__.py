a9410b0cc239c7b7909d12b7bf128a471f5ac399
Duncan Blythe <duncan@superduperdb.com>
2025-01-30 17:27:07 +0100

    [PLUGINS] Bump Version [snowflake]

--- plugins/snowflake/superduper_snowflake/__init__.py
+++ plugins/snowflake/superduper_snowflake/__init__.py
@@ -1,7 +1,7 @@
 from .vector_search import SnowflakeVectorSearcher as VectorSearcher
 from .data_backend import SnowflakeDataBackend as DataBackend
 
-__version__ = "0.5.15"
+__version__ = "0.5.16"
 
 __all__ = [
     "VectorSearcher",

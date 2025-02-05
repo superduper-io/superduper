32680e8d1dea2fba7161dfb073c2e1d117f63f14
Duncan Blythe <duncan@superduperdb.com>
2025-02-05 14:37:01 +0100

    [PLUGINS] Bump Version [snowflake]

--- plugins/snowflake/superduper_snowflake/__init__.py
+++ plugins/snowflake/superduper_snowflake/__init__.py
@@ -2,7 +2,7 @@ from .vector_search import SnowflakeVectorSearcher as VectorSearcher
 from .data_backend import SnowflakeDataBackend as DataBackend
 from .secrets import check_secret_updates
 
-__version__ = "0.5.17"
+__version__ = "0.5.18"
 
 __all__ = [
     "VectorSearcher",
--- plugins/snowflake/superduper_snowflake/secrets.py
+++ plugins/snowflake/superduper_snowflake/secrets.py
@@ -1,4 +1,8 @@
+import hashlib
 import json
+import os
+
+from superduper.base.exceptions import MissingSecretsException
 
 
 class UpdatingSecretException(Exception):
@@ -9,13 +13,21 @@ def check_secret_updates(db):
     result = db.databackend.conn.raw_sql("CALL v1.wrapper('SHOW SECRETS')")
 
     lookup = {
-        r[1]: json.loads(r[5])['status']['phase']
+        r[1]: json.loads(r[5])['status']['hash']
         for r in result
     }
 
     updating = []
     for k in lookup:
-        if lookup[k] != 'RUNNING':
+        if k not in os.environ:
+            raise MissingSecretsException(f'Secret {k} is missing')
+
+        if os.environ[k] == '':
+            raise MissingSecretsException(f'Secret {k} is empty')
+
+        value = os.environ[k]
+        target = hashlib.sha256(value.encode()).hexdigest()
+        if lookup[k] != target:
             updating.append(k)
     if updating:
-        raise UpdatingSecretException(f'Secrets {updating} are still updating')
+        raise UpdatingSecretException(f'Secrets {updating} are still updating.')

514e8ed3c410c140fd071de075cde668ffdb2295
Duncan Blythe <duncan@superduperdb.com>
2025-04-14 18:03:21 +0200

    Add check secret updates from 0.5

--- plugins/snowflake/superduper_snowflake/__init__.py
+++ plugins/snowflake/superduper_snowflake/__init__.py
@@ -1,9 +1,11 @@
 from .data_backend import SnowflakeDataBackend as DataBackend
 from .vector_search import SnowflakeVectorSearcher as VectorSearcher
+from .secrets import check_secret_updates
 
 __version__ = "0.6.0"
 
 __all__ = [
     "VectorSearcher",
     "DataBackend",
+    "check_secret_updates",
 ]
---
+++ plugins/snowflake/superduper_snowflake/secrets.py
@@ -0,0 +1,28 @@
+import hashlib
+import json
+import os
+
+from superduper.base.exceptions import MissingSecretsException
+
+
+class UpdatingSecretException(Exception):
+    ...
+
+
+def check_secret_updates(db):
+    result = db.databackend.raw_sql("CALL v1.wrapper('SHOW SECRETS IN ADMIN')")
+
+    lookup = {r[1]: json.loads(r[5])['status']['hash'] for r in result}
+
+    updating = []
+    for k in lookup:
+        if k not in os.environ:
+            raise MissingSecretsException(f'Secret {k} is missing')
+
+        value = os.environ[k]
+        target = hashlib.sha256(value.encode()).hexdigest()
+        if lookup[k] != target:
+            updating.append(k)
+
+    if updating:
+        raise UpdatingSecretException(f'Secrets {updating} are still updating.')

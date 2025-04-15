5067bbce59614d93f559739c34f562c72d34b7d5
JieguangZhou <jalonzjg@gmail.com>
2025-04-15 22:21:59 +0800

    Fix code format

--- plugins/snowflake/superduper_snowflake/__init__.py
+++ plugins/snowflake/superduper_snowflake/__init__.py
@@ -1,6 +1,6 @@
 from .data_backend import SnowflakeDataBackend as DataBackend
-from .vector_search import SnowflakeVectorSearcher as VectorSearcher
 from .secrets import check_secret_updates
+from .vector_search import SnowflakeVectorSearcher as VectorSearcher
 
 __version__ = "0.6.0"
 

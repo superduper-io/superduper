12c9a4acea7987d42798ff49b19881d4d3846c60
JieguangZhou <jalonzjg@gmail.com>
2025-04-25 16:13:41 +0800

    [PLUGINS] Bump Version [sql | mongodb | redis]

--- plugins/redis/superduper_redis/__init__.py
+++ plugins/redis/superduper_redis/__init__.py
@@ -1,6 +1,6 @@
 from .cache import RedisCache as Cache
 from .data_backend import RedisDataBackend as DataBackend
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 __all__ = ['Cache', 'DataBackend']

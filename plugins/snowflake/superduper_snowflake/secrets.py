Pinnacle-Networks/Pinnacle

replace adhoc exceptions with more well-defined counteparts
Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

plugins/snowflake/superduper_snowflake/secrets.py
@@ -2,7 +2,7 @@ import hashlib
 import json
 import os
 
-from pinnacle.base.exceptions import MissingSecretsException
+from pinnacle.base import exceptions
 
 
 class UpdatingSecretException(Exception):
@@ -21,7 +21,7 @@ def check_secret_updates(db):
     updating = []
     for k in lookup:
         if k not in os.environ:
-            raise MissingSecretsException(f'Secret {k} is missing')
+            raise exceptions.NotFound("secret", k)
 
         value = os.environ[k]
         target = hashlib.sha256(value.encode()).hexdigest()

pinnacle/base/apply.py
@@ -6,9 +6,9 @@ import click
 from rich.console import Console
 
 from pinnacle import CFG, Component, logging
+from pinnacle.base import exceptions
 from pinnacle.base.document import Document
 from pinnacle.base.event import Create, Signal, Update
-from pinnacle.base.metadata import NonExistentMetadataError
 from pinnacle.components.component import ready_status
 from pinnacle.misc.tree import dict_to_tree
 
@@ -153,7 +153,7 @@ def _wait_on_events(db, events):
             version = event.data['version']
             try:
                 db.load(component=component, identifier=identifier, version=version)
-            except NonExistentMetadataError:
+            except exceptions.NotFound:
                 pass
             else:
                 remaining -= 1
@@ -225,7 +225,7 @@ def _apply(
             apply_status = 'breaking'
             assert current.version is not None
             object.version = current.version + 1
-    except NonExistentMetadataError:
+    except exceptions.NotFound:
         apply_status = 'new'
         object.version = 0
 

pinnacle/base/metadata.py
@@ -8,43 +8,19 @@ from traceback import format_exc
 import click
 
 from pinnacle import logging
+from pinnacle.base import exceptions
 from pinnacle.base.base import Base
-from pinnacle.base.exceptions import DatabackendError
 from pinnacle.base.schema import Schema
 from pinnacle.components.cdc import CDC
 from pinnacle.components.component import Component, init_status
 from pinnacle.components.table import Table
-from pinnacle.misc.importing import import_object, isreallyinstance
+from pinnacle.misc.importing import import_object
 from pinnacle.misc.utils import merge_dicts
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
     from pinnacle.components.component import Status
 
-
-class NonExistentMetadataError(Exception):
-    """Error to raise when a component type doesn't exist.
-
-    # noqa
-    """
-
-
-class UniqueConstraintError(Exception):
-    """Unique constraint error to raise when a component exists.
-
-    # noqa
-    """
-
-
-class Pending(Base):
-    """Pending table.
-
-    #noqa
-    """
-
-    component: str
-    identifier: str
-    status: t.Dict = dc.field(default_factory=init_status)
 
 
 class Job(Base):
@@ -253,7 +229,7 @@ class MetaDataStore:
 
         try:
             r = self.db['Table'].get(identifier='Table')
-        except NonExistentMetadataError:
+        except exceptions.NotFound:
             r = None
 
         if r is None:
@@ -350,7 +326,7 @@ class MetaDataStore:
             r = self.get_component('Table', cls.__name__)
             if r is not None:
                 return
-        except NonExistentMetadataError:
+        except exceptions.NotFound:
             pass
 
         pid = self.db.databackend.id_field
@@ -416,9 +392,10 @@ class MetaDataStore:
             msg = f'Component {component} with different path {path} already exists'
             r = self.get_component('Table', component)
             if r is None:
-                raise NonExistentMetadataError
+                raise exceptions.NotFound(component, path)
+
             assert r['path'] == path, msg
-        except NonExistentMetadataError:
+        except exceptions.NotFound:
             assert path is not None
             cls = import_object(path)
             self.create(cls)
@@ -689,7 +666,6 @@ class MetaDataStore:
 
     def show_cdc_tables(self):
         """List the tables used for CDC."""
-        from pinnacle.base.document import Document
 
         metadata = self.db['Table'].execute()
 
@@ -766,9 +742,8 @@ class MetaDataStore:
             .get()
         )
         if r is None:
-            raise NonExistentMetadataError(
-                f'{identifier} does not exist in metadata for {component}'
-            )
+            raise exceptions.NotFound(component, identifier)
+
         return r['uuid']
 
     def component_version_has_parents(
@@ -799,9 +774,7 @@ class MetaDataStore:
         versions = t.filter(t['identifier'] == identifier).distinct('version')
 
         if not versions:
-            raise NonExistentMetadataError(
-                f'{identifier} does not exist in metadata for {component}'
-            )
+            raise exceptions.NotFound(component, identifier)
         return max(versions)
 
     def get_component_by_uuid(self, component: str, uuid: str):
@@ -815,15 +788,13 @@ class MetaDataStore:
             r = self.db[component].get(uuid=uuid, raw=True)
 
         if r is None:
-            raise NonExistentMetadataError(
-                f'Object {uuid} does not exist in metadata for {component}'
-            )
+            raise exceptions.NotFound(component, uuid)
 
         # TODO replace database query with cache query
         metadata = self.db['Table'].get(identifier=component)
 
         if metadata is None:
-            raise NonExistentMetadataError(f'No such Table: {component}')
+            raise exceptions.NotFound(component, uuid)
 
         path = metadata['path']
         r['_path'] = path
@@ -861,9 +832,8 @@ class MetaDataStore:
         r = self.db[component].get(identifier=identifier, version=version, raw=True)
 
         if r is None:
-            raise NonExistentMetadataError(
-                f'Object {identifier} does not exist in metadata for {component}'
-            )
+            raise exceptions.NotFound(component, identifier)
+
         r['_path'] = metadata['path']
 
         return r

pinnacle/base/query.py
@@ -17,12 +17,11 @@ import uuid
 from types import MethodType
 
 from pinnacle import CFG, logging
+from pinnacle.base import exceptions
 from pinnacle.base.base import Base
 from pinnacle.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES, KEY_PATH
 from pinnacle.base.datatype import BaseDataType
 from pinnacle.base.document import Document, _unpack
-from pinnacle.base.metadata import NonExistentMetadataError
-from pinnacle.base.schema import Schema
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
@@ -305,8 +304,8 @@ def insert(self: 'Query', documents, raw: bool = False):
     """
     try:
         _ = self.db.load('Table', self.table)
-    except NonExistentMetadataError as e:
-        raise NonExistentMetadataError(
+    except exceptions.NotFound as e:
+        raise exceptions.InternalServerError(
             f'You tried to insert into a table that does not exist: {self.table}. '
             f'To generate inline use `db.insert` instead.'
         ) from e

superduper-io/superduper

plugins/snowflake/superduper_snowflake/secrets.py
@@ -2,7 +2,7 @@ import hashlib
 import json
 import os
 
-from superduper.base.exceptions import MissingSecretsException
+from superduper.base import exceptions
 
 
 class UpdatingSecretException(Exception):
@@ -21,7 +21,7 @@ def check_secret_updates(db):
     updating = []
     for k in lookup:
         if k not in os.environ:
-            raise MissingSecretsException(f'Secret {k} is missing')
+            raise exceptions.NotFound("secret", k)
 
         value = os.environ[k]
         target = hashlib.sha256(value.encode()).hexdigest()

superduper/base/apply.py
@@ -6,9 +6,9 @@ import click
 from rich.console import Console
 
 from superduper import CFG, Component, logging
+from superduper.base import exceptions
 from superduper.base.document import Document
 from superduper.base.event import Create, Signal, Update
-from superduper.base.metadata import NonExistentMetadataError
 from superduper.components.component import ready_status
 from superduper.misc.tree import dict_to_tree
 
@@ -153,7 +153,7 @@ def _wait_on_events(db, events):
             version = event.data['version']
             try:
                 db.load(component=component, identifier=identifier, version=version)
-            except NonExistentMetadataError:
+            except exceptions.NotFound:
                 pass
             else:
                 remaining -= 1
@@ -225,7 +225,7 @@ def _apply(
             apply_status = 'breaking'
             assert current.version is not None
             object.version = current.version + 1
-    except NonExistentMetadataError:
+    except exceptions.NotFound:
         apply_status = 'new'
         object.version = 0
 

superduper/base/metadata.py
@@ -8,43 +8,19 @@ from traceback import format_exc
 import click
 
 from superduper import logging
+from superduper.base import exceptions
 from superduper.base.base import Base
-from superduper.base.exceptions import DatabackendError
 from superduper.base.schema import Schema
 from superduper.components.cdc import CDC
 from superduper.components.component import Component, init_status
 from superduper.components.table import Table
-from superduper.misc.importing import import_object, isreallyinstance
+from superduper.misc.importing import import_object
 from superduper.misc.utils import merge_dicts
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
     from superduper.components.component import Status
 
-
-class NonExistentMetadataError(Exception):
-    """Error to raise when a component type doesn't exist.
-
-    # noqa
-    """
-
-
-class UniqueConstraintError(Exception):
-    """Unique constraint error to raise when a component exists.
-
-    # noqa
-    """
-
-
-class Pending(Base):
-    """Pending table.
-
-    #noqa
-    """
-
-    component: str
-    identifier: str
-    status: t.Dict = dc.field(default_factory=init_status)
 
 
 class Job(Base):
@@ -253,7 +229,7 @@ class MetaDataStore:
 
         try:
             r = self.db['Table'].get(identifier='Table')
-        except NonExistentMetadataError:
+        except exceptions.NotFound:
             r = None
 
         if r is None:
@@ -350,7 +326,7 @@ class MetaDataStore:
             r = self.get_component('Table', cls.__name__)
             if r is not None:
                 return
-        except NonExistentMetadataError:
+        except exceptions.NotFound:
             pass
 
         pid = self.db.databackend.id_field
@@ -416,9 +392,10 @@ class MetaDataStore:
             msg = f'Component {component} with different path {path} already exists'
             r = self.get_component('Table', component)
             if r is None:
-                raise NonExistentMetadataError
+                raise exceptions.NotFound(component, path)
+
             assert r['path'] == path, msg
-        except NonExistentMetadataError:
+        except exceptions.NotFound:
             assert path is not None
             cls = import_object(path)
             self.create(cls)
@@ -689,7 +666,6 @@ class MetaDataStore:
 
     def show_cdc_tables(self):
         """List the tables used for CDC."""
-        from superduper.base.document import Document
 
         metadata = self.db['Table'].execute()
 
@@ -766,9 +742,8 @@ class MetaDataStore:
             .get()
         )
         if r is None:
-            raise NonExistentMetadataError(
-                f'{identifier} does not exist in metadata for {component}'
-            )
+            raise exceptions.NotFound(component, identifier)
+
         return r['uuid']
 
     def component_version_has_parents(
@@ -799,9 +774,7 @@ class MetaDataStore:
         versions = t.filter(t['identifier'] == identifier).distinct('version')
 
         if not versions:
-            raise NonExistentMetadataError(
-                f'{identifier} does not exist in metadata for {component}'
-            )
+            raise exceptions.NotFound(component, identifier)
         return max(versions)
 
     def get_component_by_uuid(self, component: str, uuid: str):
@@ -815,15 +788,13 @@ class MetaDataStore:
             r = self.db[component].get(uuid=uuid, raw=True)
 
         if r is None:
-            raise NonExistentMetadataError(
-                f'Object {uuid} does not exist in metadata for {component}'
-            )
+            raise exceptions.NotFound(component, uuid)
 
         # TODO replace database query with cache query
         metadata = self.db['Table'].get(identifier=component)
 
         if metadata is None:
-            raise NonExistentMetadataError(f'No such Table: {component}')
+            raise exceptions.NotFound(component, uuid)
 
         path = metadata['path']
         r['_path'] = path
@@ -861,9 +832,8 @@ class MetaDataStore:
         r = self.db[component].get(identifier=identifier, version=version, raw=True)
 
         if r is None:
-            raise NonExistentMetadataError(
-                f'Object {identifier} does not exist in metadata for {component}'
-            )
+            raise exceptions.NotFound(component, identifier)
+
         r['_path'] = metadata['path']
 
         return r

superduper/base/query.py
@@ -17,12 +17,11 @@ import uuid
 from types import MethodType
 
 from superduper import CFG, logging
+from superduper.base import exceptions
 from superduper.base.base import Base
 from superduper.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES, KEY_PATH
 from superduper.base.datatype import BaseDataType
 from superduper.base.document import Document, _unpack
-from superduper.base.metadata import NonExistentMetadataError
-from superduper.base.schema import Schema
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
@@ -305,8 +304,8 @@ def insert(self: 'Query', documents, raw: bool = False):
     """
     try:
         _ = self.db.load('Table', self.table)
-    except NonExistentMetadataError as e:
-        raise NonExistentMetadataError(
+    except exceptions.NotFound as e:
+        raise exceptions.InternalServerError(
             f'You tried to insert into a table that does not exist: {self.table}. '
             f'To generate inline use `db.insert` instead.'
         ) from e
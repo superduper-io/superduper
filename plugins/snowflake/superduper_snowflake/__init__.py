8945ca95242b51c5ad52697cc77d7934a6e779a5
Fotis Nikolaidis <nikolaidis.fotis@gmail.com>
2025-05-29 21:36:22 +0300

    update the structure of the secrets
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- plugins/snowflake/superduper_snowflake/__init__.py
+++ plugins/snowflake/superduper_snowflake/__init__.py
@@ -1,5 +1,5 @@
 from .data_backend import SnowflakeDataBackend as DataBackend
-from .secrets import check_secret_updates
+from .secrets import check_secret_updates, raise_if_secrets_pending, secrets_not_ready
 from .vector_search import SnowflakeVectorSearcher as VectorSearcher
 
 __version__ = "0.7.0"
@@ -8,4 +8,6 @@ __all__ = [
     "VectorSearcher",
     "DataBackend",
     "check_secret_updates",
+    "raise_if_secrets_pending",
+    "secrets_not_ready"
 ]

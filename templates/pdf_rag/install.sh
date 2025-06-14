19d255a63c9123bdf05307d633fad2097cdb02ca
Duncan Blythe <duncan@superduperdb.com>
2025-06-14 22:13:15 +0200

    Remove templates

--- templates/pdf_rag/install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt install -y libgl1-mesa-glx
-sudo apt install -y poppler-utils
-sudo apt install -y tesseract-ocr
-python -c 'import nltk; nltk.download("punkt"); nltk.download("averaged_perceptron_tagger"); nltk.download("punkt_tab"); nltk.download("averaged_perceptron_tagger_eng");'

20680f16b0329ebb917731e0055fe00663140a96
Duncan Blythe <duncan@superduperdb.com>
2025-08-08 11:47:11 +0200

    [PLUGINS] Bump Version [mongodb|sql|snowflake]

--- plugins/sql/superduper_sql/__init__.py
+++ plugins/sql/superduper_sql/__init__.py
@@ -1,6 +1,6 @@
 from .data_backend import SQLDatabackend as DataBackend
 from .database_listener import SQLDatabaseListener as DatabaseListener
 
-__version__ = "0.9.0"
+__version__ = "0.9.0" 
 
 __all__ = ["DataBackend", "DatabaseListener"]
